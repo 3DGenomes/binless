3a0f8c8d8c8d1463f0ff3b03f204c5ac1ecb7120
Yannick Spill <ygspill@pcb.ub.es>
2016-03-31 12:01:59 +0200

    start working on cs norm

---
+++ sparse_cs_norm.R
@@ -0,0 +1,78 @@
+library(data.table)
+library(parallel)
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+library(ggplot2)
+library(shinystan)
+library(mgcv)
+library(scam)
+
+setwd("/home/yannick/simulations/spline_stan")
+
+stan_matrix_to_datatable = function(opt, x1, x2) {
+  vals=data.table(opt)
+  vals[,x1:=x1]
+  vals[,x2:=x2]
+  melt(data.table(vals), id.vars=c("x1","x2"))
+}
+
+convert_to_simple = function(data) {
+  #convert full cs norm to simple cs norm input
+  newdata=data[,.(begin1,begin2,N,rejoined.1,rejoined.2,dangling.L.1,dangling.L.2,dangling.R.1,dangling.R.2,distance)]
+  return(newdata[,.(N=sum(N)),by=c("begin1","begin2","rejoined.1","rejoined.2","dangling.L.1","dangling.L.2",
+                                   "dangling.R.1","dangling.R.2","distance")])
+}
+
+#load data
+data=fread("cs_norm_binned_rao_HiC035_chr22.dat", stringsAsFactors = T)
+data=fread("cs_norm_binned_rao_HiC036_chr22.dat", stringsAsFactors = T)
+data=fread("cs_norm_binned_rao_HiC036_chr22_10k.dat", stringsAsFactors = T)
+data=fread("cs_norm_binned_rao_HiCall_chr22.dat", stringsAsFactors = T)
+data=fread("cs_norm_binned_caulo.dat", stringsAsFactors = T)
+
+biases=fread("caulo_toy_biases.dat")
+counts=fread("caulo_toy_counts.dat")
+data=unique(rbind(counts[,.(id=id1,pos=pos1, rejoined=rejoined.1, )],counts[,.(id=id2,pos=pos2)]))[biases,,on="id"]
+
+
+data=fread("cs_norm_caulo.dat", stringsAsFactors = T)
+counts=fread("cs_norm_rao_HiC035_chr22.dat", stringsAsFactors = T)
+data=unique(rbind(counts[,.(id=id1,pos=pos1)],counts[,.(id=id2,pos=pos2)]))[biases,,on="id"]
+data=fread("rao_HICall_chr19_35400000-35500000_biases.dat")
+setnames(data, "re.pos","pos")
+
+ggplot(melt(data,id.vars=c("id","pos")),aes(pos,value,colour=variable))+geom_point()+geom_line()+scale_y_log10()
+
+
+
+
+#fit it with stan and gam
+sm = stan_model(file = "sparse_cs_norm.stan")
+smi = stan_model(file = "gam_one_sparse.stan")
+system.time(op <- optimizing(sm, data = list(Krow=100, S=data[,.N], cutsites=data[,pos], rejoined=data[,y],
+                                             danglingL=data[,dangling.L], danglingR=data[,dangling.R]),
+                             as_vector=F, hessian=F, iter=100000, verbose=T))
+opi = optimizing(smi, data = list(N=data[,.N], K=10, y=data[,y], x=data[,pos]),
+                as_vector=F, hessian=F, iter=10000)
+                #init=list(intercept=fit$coefficients["(Intercept)"]))
+
+
+fit=gam(data=data, formula = y~s(pos,bs="ps", m=c(2,2), k=100), family=nb())
+data[,gam:=fit$fitted.values]
+
+data[,nui:=opi$par$pred]
+
+data[,nu:=exp(op$par$eRJ+1+op$par$log_nu)]
+data[,delta:=exp(op$par$log_delta)]
+#data[,fij:=op$par$decay]
+#plot result
+ggplot(melt(data,id.vars=c("id","pos")),aes(pos,value,colour=variable))+geom_point()+geom_line()+scale_y_log10()
+ggplot(melt(data,id.vars=c("id","pos")),aes(pos,value,colour=variable))+geom_point()+geom_line()+scale_y_log10()+xlim(35450000, 35475000)
+ggplot(data) + geom_line(aes(pos,f))+geom_point(aes(pos,y))+
+  geom_line(aes(pos,nu),colour="green")+ geom_line(aes(pos,gam), colour="red")+
+  geom_line(aes(pos,nui),colour="blue")
+ggplot(melt(data,id.vars=c("id","pos"))[variable%in%c("nu","nu3","nu3b","nu3c")],aes(pos,value,colour=variable))+geom_point()+geom_line()+scale_y_log10()
+
+
+
