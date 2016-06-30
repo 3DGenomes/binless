6a9f9a70fa3d100e2a5c271b9eab46dfe2848e15
Yannick Spill <yannick.spill@crg.eu>
2016-06-30 17:50:26 +0200

    crude estimate of log nu

--- bias_correlation.R
+++ bias_correlation.R
@@ -26,6 +26,56 @@ get_gc_content = function(positions, bases.up=10, bases.dn=10) {
 
 gc=get_gc_content(cs@biases[,pos])
 
+get_count_sums = function(positions, counts) {
+  cs1=counts[,sum(contact.close+contact.down+contact.far+contact.up),by=pos1][,.(pos=pos1,N=V1)]
+  cs2=counts[,sum(contact.close+contact.down+contact.far+contact.up),by=pos2][,.(pos=pos2,N=V1)]
+  setkey(cs1,pos)
+  setkey(cs2,pos)
+  pos=data.table(pos=positions, key="pos")
+  sums=rbind(cs1,cs2)[,sum(N),keyby=pos][pos]
+  return(sums[,V1])
+}
+
+get_nu_estimates = function(positions, counts) {
+  S=length(positions)
+  dt=data.table(pos=positions,sums=get_count_sums(positions, counts))
+  eC=dt[sums>0,sum(log(sums))/S-log(4*S)]
+  dt[,log_nu:=log(sums)-log(4*S)-eC]
+  min_log_nu=dt[!is.infinite(log_nu),min(log_nu)]
+  dt[is.infinite(log_nu),log_nu:=min_log_nu/10]
+  return(list(eC=eC, log_nu=dt[,log_nu]))
+}
+
+
+get_count_diffs = function(positions, counts) {
+  cs1=counts[,sum(-contact.close-contact.down+contact.far+contact.up),by=pos1][,.(pos=pos1,N=V1)]
+  cs2=counts[,sum(+contact.close-contact.down-contact.far+contact.up),by=pos2][,.(pos=pos2,N=V1)]
+  setkey(cs1,pos)
+  setkey(cs2,pos)
+  pos=data.table(pos=positions, key="pos")
+  sums=rbind(cs1,cs2)[,sum(N),keyby=pos][pos]
+  return(sums[,V1])
+}
+
+get_count_ratios = function(positions, counts) {
+  cs1=counts[,sum(contact.far+contact.up)/sum(contact.close+contact.down),by=pos1][,.(pos=pos1,N=V1)]
+  cs2=counts[,sum(contact.close+contact.up)/sum(contact.down+contact.far),by=pos2][,.(pos=pos2,N=V1)]
+  setkey(cs1,pos)
+  setkey(cs2,pos)
+  pos=data.table(pos=positions, key="pos")
+  sums=rbind(cs1,cs2)[,sum(N),keyby=pos][pos]
+  return(sums[,V1])
+}
+
+get_bias_sums = function(biases) {
+  return(biases[,dangling.L+dangling.R+rejoined])
+}
+
+get_de_diff = function(biases) {
+  return(biases[,dangling.L-dangling.R])
+}
+
+
 data=data.table(pos=cs@biases[,pos], log_nu=cs@par$log_nu, log_delta=cs@par$log_delta)
 data[,npd:=log_nu+log_delta]
 data[,nmd:=log_nu-log_delta]
@@ -38,11 +88,50 @@ data[,gc40d:=get_gc_content(pos,bases.up=0,bases.dn=40)]
 data[,gc10sym:=get_gc_content(pos,bases.up=5,bases.dn=5)]
 data[,gc10u:=get_gc_content(pos,bases.up=10,bases.dn=0)]
 data[,gc10d:=get_gc_content(pos,bases.up=0,bases.dn=10)]
-data=data[2:2014]
-pairs(data)
+data[,count.sums:=get_count_sums(pos,cs@counts)]
+data[,count.diffs:=get_count_diffs(pos,cs@counts)]
+data[,count.ratios:=get_count_ratios(pos,cs@counts)]
+data[,bias.sums:=get_bias_sums(cs@biases)]
+data[,de.diffs:=get_de_diff(cs@biases)]
+#data=data[2:2014]
+pairs(data=data, ~log_nu+log_delta+npd+nmd+count.sums+bias.sums+count.diffs+de.diffs)
 
 
 fit_nu=lm(log_nu ~ m40sym+m40u+m40d+gc40sym+gc40u+gc40d+gc10sym+gc10u+gc10d, data=data)
 fit_delta=lm(log_delta ~ m40sym+m40u+m40d+gc40sym+gc40u+gc40d+gc10sym+gc10u+gc10d, data=data)
 fit_npd=lm(npd ~ m40sym+m40u+m40d+gc40sym+gc40u+gc40d+gc10sym+gc10u+gc10d, data=data)
 fit_nmd=lm(nmd ~ m40sym+m40u+m40d+gc40sym+gc40u+gc40d+gc10sym+gc10u+gc10d, data=data)
+
+summary(lm(log_nu~count.sums+bias.sums+count.diffs+de.diffs,data=data))
+summary(lm(log_delta~count.sums+bias.sums+count.diffs+de.diffs,data=data))
+summary(lm(log_delta~count.diffs/count.sums,data=data))
+summary(lm(log_delta~I(count.diffs/count.sums),data=data))
+summary(lm(log_delta~count.diffs,data=data))
+pairs(~I(log_delta-log_nu)+count.diffs+I(count.diffs/count.sums)+log(count.ratios),data=data[!is.infinite(count.ratios)])
+
+data[,nu:=exp(log_nu)]
+data[,delta:=exp(log_delta)]
+data[,csum.std:=count.sums/mean(count.sums)]
+data[,bsum.std:=bias.sums/mean(bias.sums)]
+data[,cdiff.std:=count.diffs/count.sums]
+
+#nu vs count sum
+pairs(data=data, ~nu+log_nu+count.sums+csum.std+log(csum.std))
+ggplot(data)+geom_point(aes(log_nu,csum.std))+xlim(-2,2)+ylim(-2,2)
+summary(lm(log_nu~csum.std,data=data))
+ggplot(data)+geom_point(aes(nu,csum.std))+xlim(-2,2)+ylim(-2,2)
+summary(lm(nu~csum.std,data=data))
+ggplot(data)+geom_point(aes(log_nu,log(csum.std)))+xlim(-2,2)+ylim(-2,2)
+summary(lm(log_nu~log(count.sums),data=data))
+summary(lm(log_nu~log(csum.std)+0,data=data))
+summary(lm(log_nu~log(csum.std)+log(bsum.std)+0,data=data[bsum.std>0]))
+
+#delta vs count sums and diffs
+pairs(data=data, ~delta+log_delta+count.diffs+cdiff.std+count.sums)
+
+sm=stan_model("guess_aggreg.stan")
+op=optimizing(sm, data=list(N=cs@biases[,.N], counts_sum=data[,count.sums]), as_vector=F, hessian=F, init=0)
+
+data[,log_nu_est:=get_nu_estimates(pos,cs@counts)$log_nu]
+pairs(data=data, ~nu+log_nu+count.sums+csum.std+log(csum.std)+log_nu_est)
+summary(lm(log_nu~log_nu_est+0,data=data))
