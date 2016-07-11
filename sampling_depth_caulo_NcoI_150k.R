4d962eb1e995b37d73d5794a6f64d4b97d93c065
Yannick Spill <yannick.spill@crg.eu>
2016-07-11 12:29:27 +0200

    redo sampling depth plots with varying initial conditions

--- sampling_depth_caulo_NcoI_150k.R
+++ sampling_depth_caulo_NcoI_150k.R
@@ -33,7 +33,7 @@ foreach (nreads=c(5,10,20,30,40,50,60,75,100,200,300,400)) %do% {
 
 ### normalize different datasets on a single CPU
 registerDoParallel(cores=30)
-foreach (nreads=c(5,10,20,30,40,50,60,75,100,200,300,400)) %dopar% {
+foreach (nreads=c(5,10,20,30,40,50,60,75,100,200,300,400)) %:% foreach (lambda=c(0.01,0.1,1,10,100)) %dopar% {
   load(paste0("data/caulo_NcoI_150k_sub",nreads,"k_csnorm.RData"))
   cs@counts=fill_zeros(counts = cs@counts, biases = cs@biases)
   cs@counts[,distance:=pmin(abs(pos2-pos1), cs@settings$circularize+1-abs(pos2-pos1))]
@@ -42,7 +42,7 @@ foreach (nreads=c(5,10,20,30,40,50,60,75,100,200,300,400)) %dopar% {
   dmin=1-0.01
   dmax=150000+0.01
   init.a=system.time(init.output <- capture.output(init.op <- csnorm:::run_split_parallel_initial_guess(
-    counts=cs@counts, biases=cs@biases,
+                                         counts=cs@counts, biases=cs@biases, lambda=lambda,
     bf_per_kb=bf_per_kb, dmin=dmin, dmax=dmax, bf_per_decade=bf_per_decade, verbose=F, iter=10000)))
   a=system.time(output <- capture.output(op <- csnorm:::csnorm_fit(
     model=csnorm:::stanmodels$fit, biases=cs@biases, counts = cs@counts, dmin=dmin, dmax=dmax,
@@ -52,52 +52,56 @@ foreach (nreads=c(5,10,20,30,40,50,60,75,100,200,300,400)) %dopar% {
   init.op$runtime=init.a[1]+init.a[4]
   init.op$output=init.output
   op$par$init=init.op
+  op$par$value=op$value
   cs@par=op$par
   cs@settings = c(cs@settings, list(bf_per_kb=bf_per_kb, bf_per_decade=bf_per_decade, dmin=dmin, dmax=dmax))
   cs@pred=copy(csnorm_predict_all(cs,ncores=10,verbose=F))
   cs=postprocess(cs, resolution=10000, ncores=10, verbose=F)
   cs@binned[[1]]=iterative_normalization(cs@binned[[1]], niterations=1)
-  save(cs, file=paste0("data/caulo_NcoI_150k_sub",nreads,"k_csnorm_optimized.RData"))
+  save(cs, file=paste0("data/caulo_NcoI_150k_sub",nreads,"k_lambda",lambda,"_csnorm_optimized.RData"))
 }
 
 
 ### generate plots
 prefix="NcoI_150k"
 nreads=c(5,10,20,30,40,50,60,75,100,200,300,400)
+lambdas=c(0.01,0.1,1,10,100)
 
-
-outputs = foreach (i=nreads,.combine=rbind) %do% {
-  load(paste0("data/caulo_",prefix,"_sub",i,"k_csnorm_optimized.RData"))
-  data.table(dset=i,out=tail(cs@par$output,1), runtime=cs@par$runtime+cs@par$init$runtime, iruntime=cs@par$init$runtime,
+outputs = foreach (i=nreads,.combine=rbind) %:% foreach (j=lambdas,.combine=rbind) %do% {
+  load(paste0("data/caulo_NcoI_150k_sub",i,"k_lambda",j,"_csnorm_optimized.RData"))
+  data.table(dset=paste("bf",i,"lam",j),size=i,lambda=j, logp=cs@par$value,
+             runtime=cs@par$runtime+cs@par$init$runtime, iruntime=cs@par$init$runtime,
              lambda_nu=cs@par$lambda_nu, lambda_delta=cs@par$lambda_delta)
 }
 outputs
-summary(lm(data=outputs, log(runtime)~log(dset)))
-ggplot(outputs)+geom_point(aes(dset,runtime))+scale_x_log10()+scale_y_log10()+ylab("run time (s)")+xlab("number of reads (x1000)")+
-  ggtitle("t ~ size^-0.3")
+summary(lm(data=outputs, log(runtime)~log(size)+log(lambda)))
+ggplot(outputs)+geom_point(aes(size,runtime,colour=log(lambda)))+scale_x_log10()+scale_y_log10()+
+  ylab("run time (s)")+xlab("dataset size")+ggtitle("t ~ size^-0.36")
 ggsave(filename=paste0("images/caulo_",prefix,"_sampling_depth_runtime.png"), width=10, height=7.5)
-ggplot(melt(outputs[,.(dset,lambda_nu,lambda_delta)], id.vars="dset"))+
-  geom_line(aes(dset,value,colour=variable))+scale_x_log10()+scale_y_log10()+ylab("lambda")+xlab("number of reads (x1000)")
 
 #lFC
-lFC = foreach (i=nreads,.combine=rbind) %do% {
-  load(paste0("data/caulo_",prefix,"_sub",i,"k_csnorm_optimized.RData"))
-  get_cs_binned(cs,1,"CS")[,.(dset=i,lFC)]
+lFC = foreach (i=nreads,.combine=rbind) %:%
+  foreach (j=lambdas,.combine=function(x,y){if (x$logp[1]<y$logp[1]){return(y)}else{return(x)}}) %do% {
+  load(paste0("data/caulo_NcoI_150k_sub",i,"k_lambda",j,"_csnorm_optimized.RData"))
+  get_cs_binned(cs,1,"CS")[,.(dset=paste("bf",i,"lam",j),size=i,lambda=j, logp=cs@par$value, lFC)]
 }
 load(paste0("data/caulo_",prefix,"_csnorm_optimized.RData"))
-lFC=rbind(lFC,get_cs_binned(cs,1,"CS")[,.(dset="all",lFC)])
-lFC[,dset:=ordered(dset,levels=c("all",nreads))]
+lFC=rbind(lFC,get_cs_binned(cs,1,"CS")[,.(dset="all",size="all", lambda=1, logp=-1, lFC)])
+lFC[,dset:=ordered(size,levels=c("all",nreads))]
 ggplot(lFC)+geom_density(aes(lFC,colour=dset))
 ggsave(filename=paste0("images/caulo_",prefix,"_sampling_depth_lFC.png"), width=10, height=7.5)
 
 #normalized matrices
-mat = foreach (i=nreads,.combine=rbind) %do% {
-  load(paste0("data/caulo_",prefix,"_sub",i,"k_csnorm_optimized.RData"))
-  get_cs_binned(cs,1,"CS")[,.(dset=i,begin1,begin2,normalized,is.interaction,prob.observed.gt.expected)]
+mat = foreach (i=nreads,.combine=rbind) %:%
+  foreach (j=lambdas,.combine=function(x,y){if (x$logp[1]<y$logp[1]){return(y)}else{return(x)}}) %do% {
+    load(paste0("data/caulo_NcoI_150k_sub",i,"k_lambda",j,"_csnorm_optimized.RData"))
+    get_cs_binned(cs,1,"CS")[,.(dset=paste("bf",i,"lam",j),size=i,lambda=j, logp=cs@par$value,
+                                begin1,begin2,normalized,is.interaction,prob.observed.gt.expected)]
 }
 load(paste0("data/caulo_",prefix,"_csnorm_optimized.RData"))
-mat=rbind(mat,get_cs_binned(cs,1,"CS")[,.(dset="all",begin1,begin2,normalized,is.interaction,prob.observed.gt.expected)])
-mat[,dset:=ordered(dset,levels=c("all",nreads))]
+mat=rbind(mat,get_cs_binned(cs,1,"CS")[,.(dset="all",size="all",lambda=1, logp=-1,
+                                          begin1,begin2,normalized,is.interaction,prob.observed.gt.expected)])
+mat[,dset:=ordered(size,levels=c("all",nreads))]
 ggplot(mat[dset!=300])+
   geom_raster(aes(begin1,begin2,fill=log(normalized)))+
   geom_raster(aes(begin2,begin1,fill=log(normalized)))+
@@ -107,38 +111,44 @@ ggplot(mat[dset!=300])+
 ggsave(filename=paste0("images/caulo_",prefix,"_sampling_depth_normalized.png"), width=10, height=7.5)
 
 #nu and delta: init
-nu.init = foreach (i=nreads,.combine=rbind) %do% {
-  load(paste0("data/caulo_",prefix,"_sub",i,"k_csnorm_optimized.RData"))
-  csnorm:::generate_genomic_biases(biases=cs@biases, beta_nu=cs@par$init$beta_nu, beta_delta=cs@par$init$beta_delta,
+registerDoParallel(cores=30)
+nu.init = foreach (i=nreads,.combine=rbind) %:%
+  foreach (j=lambdas,.combine=function(x,y){if (x$logp[1]<y$logp[1]){return(y)}else{return(x)}}) %dopar% {
+    load(paste0("data/caulo_NcoI_150k_sub",i,"k_lambda",j,"_csnorm_optimized.RData"))
+    csnorm:::generate_genomic_biases(biases=cs@biases, beta_nu=cs@par$init$beta_nu, beta_delta=cs@par$init$beta_delta,
                                    bf_per_kb=cs@settings$bf_per_kb, points_per_kb = 100)[
-                                     ,.(pos,log_nu,log_delta,dset=i)]
+                                     ,.(pos,log_nu,log_delta,size=i,lambda=j, logp=cs@par$value)]
 }
 nuref=csnorm:::generate_genomic_biases(biases=cs@biases, beta_nu=cs@par$beta_nu, beta_delta=cs@par$beta_delta,
                                        bf_per_kb=cs@settings$bf_per_kb, points_per_kb = 100)[
-                                         ,.(pos,log_nu_ref=log_nu,log_delta_ref=log_delta)]
-nu.init=nuref[nu.init]
-nu.init[,dset:=ordered(dset,levels=nreads)]
+                                         ,.(pos,log_nu_ref=log_nu,log_delta_ref=log_delta,
+                                            dset="all",size="all",lambda=1, logp=-1)]
+nu.init[,c("log_nu_ref","log_delta_ref"):=nuref[,.(log_nu_ref,log_delta_ref)],by=size]
+nu.init[,dset:=ordered(size,levels=nreads)]
 ggplot(nu.init[,.(dset,pos,nu=exp(log_nu),nuref=exp(log_nu_ref))])+
   geom_line(aes(pos,nu),colour="black")+geom_line(aes(pos,nuref),colour="red")+facet_wrap(~dset)+ylim(0,2)
 ggsave(filename=paste0("images/caulo_",prefix,"_sampling_depth_nu_init.png"), width=10, height=7.5)
-ggplot(nu.init[,.(dset,pos,delta=exp(log_delta),nuref=exp(log_delta_ref))])+
-  geom_line(aes(pos,delta),colour="black")+geom_line(aes(pos,nuref),colour="red")+facet_wrap(~dset)+ylim(0,2)
+ggplot(nu.init[,.(dset,pos,delta=exp(log_delta),deltaref=exp(log_delta_ref))])+
+  geom_line(aes(pos,delta),colour="black")+geom_line(aes(pos,deltaref),colour="red")+facet_wrap(~dset)+ylim(0,2)
 ggsave(filename=paste0("images/caulo_",prefix,"_sampling_depth_delta_init.png"), width=10, height=7.5)
 
 
 #nu and delta: plots and correlation
-nu = foreach (i=nreads,.combine=rbind) %do% {
-  load(paste0("data/caulo_",prefix,"_sub",i,"k_csnorm_optimized.RData"))
-  csnorm:::generate_genomic_biases(biases=cs@biases, beta_nu=cs@par$beta_nu, beta_delta=cs@par$beta_delta,
+registerDoParallel(cores=30)
+nu = foreach (i=nreads,.combine=rbind) %:%
+  foreach (j=lambdas,.combine=function(x,y){if (x$logp[1]<y$logp[1]){return(y)}else{return(x)}}) %dopar% {
+    load(paste0("data/caulo_NcoI_150k_sub",i,"k_lambda",j,"_csnorm_optimized.RData"))
+    csnorm:::generate_genomic_biases(biases=cs@biases, beta_nu=cs@par$beta_nu, beta_delta=cs@par$beta_delta,
                                    bf_per_kb=cs@settings$bf_per_kb, points_per_kb = 100)[
-                                     ,.(pos,log_nu,log_delta,dset=i)]
+                                     ,.(pos,log_nu,log_delta,size=i,lambda=j, logp=cs@par$value)]
 }
 load(paste0("data/caulo_",prefix,"_csnorm_optimized.RData"))
 nuref=csnorm:::generate_genomic_biases(biases=cs@biases, beta_nu=cs@par$beta_nu, beta_delta=cs@par$beta_delta,
                                        bf_per_kb=cs@settings$bf_per_kb, points_per_kb = 100)[
-                                         ,.(pos,log_nu_ref=log_nu,log_delta_ref=log_delta)]
-nu=nuref[nu]
-nu[,dset:=ordered(dset,levels=nreads)]
+                                         ,.(pos,log_nu_ref=log_nu,log_delta_ref=log_delta,
+                                            dset="all",size="all",lambda=1, logp=-1)]
+nu[,c("log_nu_ref","log_delta_ref"):=nuref[,.(log_nu_ref,log_delta_ref)],by=size]
+nu[,dset:=ordered(size,levels=nreads)]
 ggplot(nu[,.(dset,pos,nu=exp(log_nu),nuref=exp(log_nu_ref))])+
   geom_line(aes(pos,nu),colour="black")+geom_line(aes(pos,nuref),colour="red")+facet_wrap(~dset)+ylim(0,2)
 ggsave(filename=paste0("images/caulo_",prefix,"_sampling_depth_nu.png"), width=10, height=7.5)
@@ -163,12 +173,13 @@ ggplot(fpts)+geom_point(aes(dset,fpt/1000))+geom_hline(aes(yintercept=fptref/100
 ggsave(filename=paste0("images/caulo_",prefix,"_sampling_depth_nu_delta_fpt.png"), width=10, height=7.5)
 
 #diagonal decay
-decay = foreach (i=nreads,.combine=rbind,.export="data.table") %do% {
-  load(paste0("data/caulo_",prefix,"_sub",i,"k_csnorm_optimized.RData"))
-  data.table(dist=cs@counts[,distance],decay=exp(cs@par$log_decay), dset=i,key="dist")
+decay = foreach (i=nreads,.combine=rbind) %:%
+  foreach (j=lambdas,.combine=function(x,y){if (x$logp[1]<y$logp[1]){return(y)}else{return(x)}}) %dopar% {
+    load(paste0("data/caulo_NcoI_150k_sub",i,"k_lambda",j,"_csnorm_optimized.RData"))
+    data.table(dist=cs@counts[,distance],decay=exp(cs@par$log_decay),logp=cs@par$value, dset=i,key="dist")
 }
 load(paste0("data/caulo_",prefix,"_csnorm_optimized.RData"))
-decay=rbind(decay,data.table(dist=cs@counts[,distance],decay=exp(cs@par$log_decay), dset="all",key="dist"))
+decay=rbind(decay,data.table(dist=cs@counts[,distance],decay=exp(cs@par$log_decay),logp=-1, dset="all",key="dist"))
 decay[,dset:=ordered(dset,levels=c("all",nreads))]
 decay[,decay:=decay/exp(mean(log(decay))),by=dset]
 decay=decay[dist>100]
