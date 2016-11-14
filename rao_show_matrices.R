6f074e8ba89d65d961c89e8e198a9029dd9e6a9f
Yannick Spill <yannick.spill@crg.eu>
2016-11-14 14:00:58 +0100

    scripting

--- rao_show_matrices.R
+++ rao_show_matrices.R
@@ -7,7 +7,7 @@ library(rstan)
 
 setwd("/home/yannick/simulations/cs_norm")
 
-sub="GM12878_FOXP1_oldmat"
+sub="GM12878_FOXP1_newmat_sigmainf"
 
 #normalize
 load(paste0("data/rao_HiCall_",sub,"_csdata.RData"))
@@ -16,25 +16,27 @@ cs = run_gauss(cs, bf_per_kb=3, bf_per_decade=10, bins_per_bf=10, ngibbs = 20, i
 save(cs,file=paste0("data/rao_HiCall_",sub,"_csnorm_optimized_gauss.RData"))
 
 #bin
-resolution=20000
+resolution=50000
 cs=bin_all_datasets(cs, resolution=resolution, verbose=T, ice=100, ncores=ncores)
+cs=group_datasets(cs, resolution=50000, group="condition")
 cs=detect_interactions(cs, resolution=resolution, group="all", threshold=0.95, ncores=ncores)
 
-#icelike matrix
+#normalized matrix
 mat=get_matrices(cs, resolution=resolution, group="all")
 ggplot(mat)+
-  geom_raster(aes(begin1,begin2,fill=log(icelike)))+
-  geom_raster(aes(begin2,begin1,fill=log(icelike)))+
+  geom_raster(aes(begin1,begin2,fill=log(normalized)))+
+  geom_raster(aes(begin2,begin1,fill=log(normalized)))+
   scale_fill_gradient(low="white", high="black")+
   theme_bw()+theme(legend.position = "none", axis.title=element_blank())
-ggsave(filename=paste0("images/rao_HiCall_",sub,"_icelike_",resolution/1000,"kb.pdf"), width=10,height=9)
-#normalized
-ggplot(mat)+
-  geom_raster(aes(begin1,begin2,fill=-log(normalized)))+
-  geom_raster(aes(begin2,begin1,fill=-log(normalized)))+
-  scale_fill_gradient2()+
-  theme_bw()+theme(legend.position = "none", axis.title=element_blank())
 ggsave(filename=paste0("images/rao_HiCall_",sub,"_normalized_",resolution/1000,"kb.pdf"), width=10,height=9)
+#signal
+ggplot(mat)+
+  geom_raster(aes(begin1,begin2,fill=-log(signal)))+
+  geom_raster(aes(begin2,begin1,fill=-log(signal)))+
+  theme_bw()+theme(legend.position = "none", axis.title=element_blank())+
+  #scale_fill_gradient(low="black", high="white")
+  scale_fill_gradient2()
+ggsave(filename=paste0("images/rao_HiCall_",sub,"_signal_",resolution/1000,"kb.pdf"), width=10,height=9)
 #observed
 ggplot(mat)+
   geom_raster(aes(begin1,begin2,fill=log(observed)))+
