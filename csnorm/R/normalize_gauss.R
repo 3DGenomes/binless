b3aab4bb87143bc2e70a72c6c04bacf4c1211afb
Yannick Spill <yannick.spill@crg.eu>
2016-11-24 20:06:32 +0100

    merge performance and outer iterations in a single call

--- csnorm/R/normalize_gauss.R
+++ csnorm/R/normalize_gauss.R
@@ -4,6 +4,29 @@ NULL
 #' Compute decay etahat and weights using data as initial guess
 #' @keywords internal
 #' 
+fill_parameters_perf = function(cs, dispersion=10, lambda=1, fit.decay=T, fit.genomic=T, fit.disp=T) {
+  nBiases=cs@design[,uniqueN(genomic)]
+  Decays=cs@design[,uniqueN(decay)]
+  init=list(alpha=dispersion)
+  if (fit.decay==F) {
+    Kdiag=round((log10(cs@settings$dmax)-log10(cs@settings$dmin))*cs@settings$bf_per_decade)
+    beta_diag=matrix(rep(seq(0.1,1,length.out = Kdiag-1), each=Decays), Decays, Kdiag-1)
+    init=c(init,list(beta_diag=beta_diag, lambda_diag=array(lambda,dim=Decays), log_decay=rep(0,cs@counts[,.N])))
+  }
+  if (fit.genomic==F) {
+    init=c(init,list(log_iota=array(0,dim=cs@biases[,.N]), log_rho=array(0,dim=cs@biases[,.N]),
+                     lambda_iota=array(lambda,dim=nBiases), lambda_rho=array(lambda,dim=nBiases)))
+  }
+  if (fit.genomic==F || fit.decay==F || fit.disp==F) {
+    init=c(init,list(eC=array(0,dim=cs@design[,.N]),  eRJ=array(0,dim=cs@design[,.N]),  eDE=array(0,dim=cs@design[,.N])))
+  }
+  cs@par=init
+  cs
+}
+
+#' Compute decay etahat and weights using data as initial guess
+#' @keywords internal
+#' 
 fill_parameters_outer = function(cs, dispersion=10, lambda=1, fit.decay=T, fit.genomic=T, fit.disp=T) {
   nBiases=cs@design[,uniqueN(genomic)]
   Decays=cs@design[,uniqueN(decay)]
@@ -27,14 +50,14 @@ fill_parameters_outer = function(cs, dispersion=10, lambda=1, fit.decay=T, fit.g
 #' Compute decay etahat and weights using data as initial guess
 #' @keywords internal
 #' 
-csnorm_gauss_decay_muhat_data_outer = function(cs, pseudocount=1e-2) {
+csnorm_gauss_decay_muhat_data = function(cs, pseudocount=1e-2) {
   #bin distances
   stepsz=1/(cs@settings$bins_per_bf*cs@settings$bf_per_decade)
   dbins=10**seq(log10(cs@settings$dmin),log10(cs@settings$dmax)+stepsz,stepsz)
   csd=cs@counts[,.(name,distance,
-                dbin=cut(distance,dbins,ordered_result=T,right=F,include.lowest=T,dig.lab=12),
-                kappahat=log(contact.close+contact.far+contact.up+contact.down+pseudocount),
-                var=1/(contact.close+contact.far+contact.up+contact.down+pseudocount)+1/cs@par$alpha)]
+                   dbin=cut(distance,dbins,ordered_result=T,right=F,include.lowest=T,dig.lab=12),
+                   kappahat=log(contact.close+contact.far+contact.up+contact.down+pseudocount),
+                   var=1/(contact.close+contact.far+contact.up+contact.down+pseudocount)+1/cs@par$alpha)]
   csd = csd[,.(distance=exp(mean(log(distance))), kappahat=sum(kappahat/var)/sum(1/var),
                std=1/sqrt(sum(1/var)), weight=4*.N), keyby=c("name", "dbin")]
   csd
@@ -43,7 +66,7 @@ csnorm_gauss_decay_muhat_data_outer = function(cs, pseudocount=1e-2) {
 #' Compute decay etahat and weights using previous mean
 #' @keywords internal
 #' 
-csnorm_gauss_decay_muhat_mean_outer = function(cs) {
+csnorm_gauss_decay_muhat_mean = function(cs) {
   #add bias informations to counts
   init=cs@par
   csub=copy(cs@counts)
@@ -78,7 +101,8 @@ csnorm_gauss_decay_muhat_mean_outer = function(cs) {
 #' Single-cpu simplified fitting for iota and rho
 #' @keywords internal
 #' 
-csnorm_gauss_decay_outer = function(cs, verbose=T, init.mean="mean", init_alpha=1e-7) {
+csnorm_gauss_decay = function(cs, verbose=T, init.mean="mean", init_alpha=1e-7, type=c("outer","perf")) {
+  type=match.arg(type)
   if (init.mean=="mean") {
     csd = csnorm:::csnorm_gauss_decay_muhat_mean(cs)
   } else {
@@ -90,9 +114,15 @@ csnorm_gauss_decay_outer = function(cs, verbose=T, init.mean="mean", init_alpha=
   data=list(Dsets=cs@design[,.N], Decays=cs@design[,uniqueN(decay)], XD=as.array(cs@design[,decay]),
             Kdiag=Kdiag, dmin=cs@settings$dmin, dmax=cs@settings$dmax, N=csd[,.N], cbegin=cbegin,
             kappa_hat=csd[,kappahat], sdl=csd[,std], dist=csd[,distance],
-            weight=csd[,weight], lambda_diag=as.array(cs@par$lambda_diag))
+            weight=csd[,weight])
+  if (type=="outer") {
+    data$lambda_diag=as.array(cs@par$lambda_diag)
+    model=csnorm:::stanmodels$gauss_decay_outer
+  } else {
+    model=csnorm:::stanmodels$gauss_decay_perf
+  }
   #optimize from scratch, to avoid getting stuck. Slower but more robust
-  op=optimize_stan_model(model=csnorm:::stanmodels$gauss_decay_outer, data=data, iter=cs@settings$iter,
+  op=optimize_stan_model(model=model, data=data, iter=cs@settings$iter,
                          verbose=verbose, init=0, init_alpha=init_alpha)
   #make nice decay data table
   dmat=csd[,.(name,distance,kappahat,std,ncounts=weight,kappa=op$par$log_mean_counts)]
@@ -117,7 +147,7 @@ csnorm_gauss_decay_outer = function(cs, verbose=T, init.mean="mean", init_alpha=
 #' Compute genomic etahat and weights using data as initial guess
 #' @keywords internal
 #' 
-csnorm_gauss_genomic_muhat_data_outer = function(cs, pseudocount=1e-2) {
+csnorm_gauss_genomic_muhat_data = function(cs, pseudocount=1e-2) {
   dispersion=cs@par$alpha
   bts=rbind(cs@biases[,.(name,id,pos,cat="dangling L", etahat=log(dangling.L+pseudocount), std=sqrt(1/(dangling.L+pseudocount)+1/dispersion))],
             cs@biases[,.(name,id,pos,cat="dangling R", etahat=log(dangling.R+pseudocount), std=sqrt(1/(dangling.R+pseudocount)+1/dispersion))],
@@ -141,7 +171,7 @@ csnorm_gauss_genomic_muhat_data_outer = function(cs, pseudocount=1e-2) {
 #' Compute genomic etahat and weights using previous mean
 #' @keywords internal
 #' 
-csnorm_gauss_genomic_muhat_mean_outer = function(cs) {
+csnorm_gauss_genomic_muhat_mean = function(cs) {
   #compute bias means
   init=cs@par
   bsub=copy(cs@biases)
@@ -189,12 +219,14 @@ csnorm_gauss_genomic_muhat_mean_outer = function(cs) {
   return(list(bts=bts,cts=cts))
 }
 
+
 #' Single-cpu simplified fitting for iota and rho
 #' @param if a single value, use data for estimate of mu and that value as a
 #'   dispersion, otherwise it's a list with parameters to compute the mean from
 #' @keywords internal
 #'   
-csnorm_gauss_genomic_outer = function(cs, verbose=T, init.mean="mean", init_alpha=1e-7) {
+csnorm_gauss_genomic = function(cs, verbose=T, init.mean="mean", init_alpha=1e-7, type=c("perf","outer")) {
+  type=match.arg(type)
   if (init.mean=="mean") {
     a = csnorm:::csnorm_gauss_genomic_muhat_mean(cs)
   } else {
@@ -211,9 +243,15 @@ csnorm_gauss_genomic_outer = function(cs, verbose=T, init.mean="mean", init_alph
             eta_hat_DL=bts[cat=="dangling L",etahat], sd_DL=bts[cat=="dangling L",std],
             eta_hat_DR=bts[cat=="dangling R",etahat], sd_DR=bts[cat=="dangling R",std],
             eta_hat_L=cts[cat=="contact L",etahat], sd_L=cts[cat=="contact L",std],
-            eta_hat_R=cts[cat=="contact R",etahat], sd_R=cts[cat=="contact R",std],
-            lambda_iota=as.array(cs@par$lambda_iota), lambda_rho=as.array(cs@par$lambda_rho))
-  op=optimize_stan_model(model=csnorm:::stanmodels$gauss_genomic_outer, data=data, iter=cs@settings$iter,
+            eta_hat_R=cts[cat=="contact R",etahat], sd_R=cts[cat=="contact R",std])
+  if (type=="outer") {
+    data$lambda_iota=as.array(cs@par$lambda_iota)
+    data$lambda_rho=as.array(cs@par$lambda_rho)
+    model=csnorm:::stanmodels$gauss_genomic_outer
+  } else {
+    model=csnorm:::stanmodels$gauss_genomic_perf
+  }
+  op=optimize_stan_model(model=model, data=data, iter=cs@settings$iter,
                          verbose=verbose, init=0, init_alpha=init_alpha)
   #make nice output table
   bout=rbind(bts[cat=="dangling L",.(cat, name, id, pos, etahat, std, eta=op$par$log_mean_DL)],
@@ -238,14 +276,14 @@ csnorm_gauss_genomic_outer = function(cs, verbose=T, init.mean="mean", init_alph
 #' Single-cpu simplified fitting for exposures and dispersion
 #' @keywords internal
 #' 
-csnorm_gauss_dispersion_outer = function(cs, counts, weight=cs@design[,.(name,wt=1)], verbose=T, init_alpha=1e-7) {
-  Krow=round(cs@biases[,(max(pos)-min(pos))/1000*cs@settings$bf_per_kb])
+csnorm_gauss_dispersion = function(cs, counts, weight=cs@design[,.(name,wt=1)], verbose=T, init_alpha=1e-7, type=c("outer","perf")) {
+  type=match.arg(type)
   Kdiag=round((log10(cs@settings$dmax)-log10(cs@settings$dmin))* cs@settings$bf_per_decade)
   bbegin=c(1,cs@biases[,.(name,row=.I)][name!=shift(name),row],cs@biases[,.N+1])
   cbegin=c(1,counts[,.(name,row=.I)][name!=shift(name),row],counts[,.N+1])
   data = list( Dsets=cs@design[,.N], Biases=cs@design[,uniqueN(genomic)], Decays=cs@design[,uniqueN(decay)],
                XB=as.array(cs@design[,genomic]), XD=as.array(cs@design[,decay]),
-               Krow=Krow, SD=cs@biases[,.N], bbegin=bbegin,
+               SD=cs@biases[,.N], bbegin=bbegin,
                cutsitesD=cs@biases[,pos], rejoined=cs@biases[,rejoined],
                danglingL=cs@biases[,dangling.L], danglingR=cs@biases[,dangling.R],
                Kdiag=Kdiag, dmin=cs@settings$dmin, dmax=cs@settings$dmax,
@@ -253,20 +291,31 @@ csnorm_gauss_dispersion_outer = function(cs, counts, weight=cs@design[,.(name,wt
                cidx=t(data.matrix(counts[,.(id1,id2)])), dist=counts[,distance],
                counts_close=counts[,contact.close], counts_far=counts[,contact.far],
                counts_up=counts[,contact.up], counts_down=counts[,contact.down],
-               weight=as.array(weight[,wt]), beta_iota=cs@par$beta_iota, beta_rho=cs@par$beta_rho,
-               beta_diag=cs@par$beta_diag)
-  op=optimize_stan_model(model=csnorm:::stanmodels$gauss_dispersion_outer, data=data, iter=cs@settings$iter,
-                         verbose=verbose, init=cs@par, init_alpha=init_alpha)
-  #update par slot
-  op$par$value=op$value
-  cs@par=modifyList(cs@par, op$par[c("eC","eRJ","eDE","alpha","value","lambda_iota","lambda_rho","lambda_diag")])
+               weight=as.array(weight[,wt]), beta_diag=cs@par$beta_diag)
+  if (type=="outer") {
+    data$Krow=round(cs@biases[,(max(pos)-min(pos))/1000*cs@settings$bf_per_kb])
+    data$beta_iota=cs@par$beta_iota
+    data$beta_rho=cs@par$beta_rho
+    op=optimize_stan_model(model=csnorm:::stanmodels$gauss_dispersion_outer, data=data, iter=cs@settings$iter,
+                           verbose=verbose, init=cs@par, init_alpha=init_alpha)
+    op$par$value=op$value
+    cs@par=modifyList(cs@par, op$par[c("eC","eRJ","eDE","alpha","value","lambda_iota","lambda_rho","lambda_diag")])
+  } else {
+    data$log_iota=cs@par$log_iota
+    data$log_rho=cs@par$log_rho
+    op=optimize_stan_model(model=csnorm:::stanmodels$gauss_dispersion_perf, data=data, iter=cs@settings$iter,
+                           verbose=verbose, init=cs@par, init_alpha=init_alpha)
+    op$par$value=op$value
+    cs@par=modifyList(cs@par, op$par[c("eC","eRJ","eDE","alpha","value")])
+  }
   return(cs)
 }
+
 
 #' Single-cpu simplified fitting for exposures and dispersion
 #' @keywords internal
 #' 
-has_converged_outer = function(cs) {
+has_converged = function(cs) {
   params=cs@diagnostics$params
   if (params[,.N]<=3) return(FALSE)
   tol=cs@settings$tol.obj
@@ -275,7 +324,29 @@ has_converged_outer = function(cs) {
   return(all(delta<tol))
 }
 
-#' Cut-site normalization (normal approximation, outer iteration)
+
+#' Diagnostics plots to monitor convergence of normalization (gaussian
+#' approximation)
+#' 
+#' @param cs a normalized CSnorm object
+#'   
+#' @return Two plots in a list. The first is for the three log-likelihoods, the
+#'   second is for the parameters.
+#' @export
+#' 
+#' @examples
+plot_diagnostics = function(cs) {
+  plot=ggplot(cs@diagnostics$params[,.(step,leg,value,out.last)])+
+    geom_line(aes(step,value))+geom_point(aes(step,value,colour=out.last))+facet_wrap(~leg, scales = "free")+
+    theme(legend.position="bottom")
+  vars=foreach(var=c("eC","eRJ","eDE","alpha","lambda_iota","lambda_rho"),
+               trans=(c("exp","exp","exp",NA,"log","log")),.combine=rbind) %do% get_all_values(cs,var,trans)
+  plot2=ggplot(vars)+geom_line(aes(step,value))+
+    geom_point(aes(step,value,colour=leg))+facet_wrap(~variable, scales = "free_y")
+  return(list(plot=plot,plot2=plot2))
+}
+
+#' Cut-site normalization (normal approximation)
 #' 
 #' Alternates two approximations to the exact model, fitting the diagonal decay
 #' and iota/rho.
@@ -302,15 +373,21 @@ has_converged_outer = function(cs) {
 #'   line search.
 #' @param init.dispersion positive numeric. Value of the dispersion to use initially.
 #' @param tol.obj positive numeric (default 1e-1). Convergence tolerance on changes in the three likelihoods.
+#' @param type character. Type of iteration to perform. The outer iteration ("outer", default) optimizes
+#'   penalties with the dispersion parameter. The performance iteration ("perf") optimizes penalties with
+#'   the biases. It is recommended to try performance iteration, but if convergence fails to revert to outer
+#'   iteration.
 #'   
 #' @return A csnorm object
 #' @export
 #' 
 #' @examples
 #' 
-run_gauss_outer = function(cs, init=NULL, bf_per_kb=1, bf_per_decade=20, bins_per_bf=10,
+run_gauss = function(cs, init=NULL, bf_per_kb=1, bf_per_decade=20, bins_per_bf=10,
                      ngibbs = 3, iter=10000, fit.decay=T, fit.genomic=T, fit.disp=T,
-                     verbose=T, ncounts=100000, init_alpha=1e-7, init.dispersion=10, tol.obj=1e-1) {
+                     verbose=T, ncounts=100000, init_alpha=1e-7, init.dispersion=10,
+                     tol.obj=1e-1, type="outer") {
+  type=match.arg(type,c("outer","perf"))
   #clean object if dirty
   cs@par=list() #in case we have a weird object
   cs@binned=list()
@@ -337,8 +414,13 @@ run_gauss_outer = function(cs, init=NULL, bf_per_kb=1, bf_per_decade=20, bins_pe
     cs@diagnostics=list()
     laststep=0
     init.mean="data"
-    cs=fill_parameters(cs, dispersion=init.dispersion, fit.decay=fit.decay,
-                       fit.genomic=fit.genomic, fit.disp=fit.disp)
+    if (type=="outer") {
+      cs=fill_parameters_outer(cs, dispersion=init.dispersion, fit.decay=fit.decay,
+                               fit.genomic=fit.genomic, fit.disp=fit.disp)
+    } else {
+      cs=fill_parameters_perf(cs, dispersion=init.dispersion, fit.decay=fit.decay,
+                               fit.genomic=fit.genomic, fit.disp=fit.disp)
+    }
   } else {
     if (verbose==T) cat("Using provided initial guess\n")
     if (is.data.table(cs@diagnostics$params)) laststep = cs@diagnostics$params[,max(step)] else laststep = 0
@@ -352,14 +434,16 @@ run_gauss_outer = function(cs, init=NULL, bf_per_kb=1, bf_per_decade=20, bins_pe
     #fit diagonal decay given iota and rho
     if (fit.decay==T) {
       if (verbose==T) cat("Gibbs",i,": Decay ")
-      a=system.time(output <- capture.output(cs <- csnorm:::csnorm_gauss_decay(cs, init.mean=init.mean, init_alpha=init_alpha)))
+      a=system.time(output <- capture.output(cs <- csnorm:::csnorm_gauss_decay(cs, init.mean=init.mean,
+                                                                               init_alpha=init_alpha, type=type)))
       cs@diagnostics$params = csnorm:::update_diagnostics(cs, step=i, leg="decay", out=output, runtime=a[1]+a[4])
       if (verbose==T) cat("log-likelihood = ",cs@par$value, "\n")
     }
     #fit iota and rho given diagonal decay
     if (fit.genomic==T) {
       if (verbose==T) cat("Gibbs",i,": Genomic ")
-      a=system.time(output <- capture.output(cs <- csnorm:::csnorm_gauss_genomic(cs, init.mean=init.mean, init_alpha=init_alpha)))
+      a=system.time(output <- capture.output(cs <- csnorm:::csnorm_gauss_genomic(cs, init.mean=init.mean,
+                                                                                 init_alpha=init_alpha, type=type)))
       cs@diagnostics$params = csnorm:::update_diagnostics(cs, step=i, leg="bias", out=output, runtime=a[1]+a[4])
       if (verbose==T) cat("log-likelihood = ",cs@par$value, "\n")
     }
@@ -368,11 +452,11 @@ run_gauss_outer = function(cs, init=NULL, bf_per_kb=1, bf_per_decade=20, bins_pe
       #fit exposures and dispersion
       if (verbose==T) cat("Gibbs",i,": Remaining parameters ")
       a=system.time(output <- capture.output(cs <- csnorm:::csnorm_gauss_dispersion(cs, counts=subcounts, weight=subcounts.weight,
-                                                                                    init_alpha=init_alpha)))
+                                                                                    init_alpha=init_alpha, type=type)))
       cs@diagnostics$params = csnorm:::update_diagnostics(cs, step=i, leg="disp", out=output, runtime=a[1]+a[4])
-      if (verbose==T) cat("log-likelihood = ",cs@par$value," dispersion = ",cs@par$alpha,
-                          " lambda_iota = ",cs@par$lambda_iota, "\n")
+      if (verbose==T) cat("log-likelihood = ",cs@par$value,"\n")
     }
+    if (verbose==T) cat("Gibbs",i,": dispersion = ",cs@par$alpha, " lambda_iota = ",cs@par$lambda_iota, "\n")
     if (has_converged(cs)) break
   }
   if (verbose==T) cat("Done\n")
@@ -381,23 +465,3 @@ run_gauss_outer = function(cs, init=NULL, bf_per_kb=1, bf_per_decade=20, bins_pe
   return(cs)
 }
 
-#' Diagnostics plots to monitor convergence of normalization (gaussian
-#' approximation)
-#' 
-#' @param cs a normalized CSnorm object
-#'   
-#' @return Two plots in a list. The first is for the three log-likelihoods, the
-#'   second is for the parameters.
-#' @export
-#' 
-#' @examples
-plot_diagnostics_outer = function(cs) {
-  plot=ggplot(cs@diagnostics$params[,.(step,leg,value,out.last)])+
-    geom_line(aes(step,value))+geom_point(aes(step,value,colour=out.last))+facet_wrap(~leg, scales = "free")+
-    theme(legend.position="bottom")
-  vars=foreach(var=c("eC","eRJ","eDE","alpha","lambda_iota","lambda_rho"),
-               trans=(c("exp","exp","exp",NA,"log","log")),.combine=rbind) %do% get_all_values(cs,var,trans)
-  plot2=ggplot(vars)+geom_line(aes(step,value))+
-    geom_point(aes(step,value,colour=leg))+facet_wrap(~variable, scales = "free_y")
-  return(list(plot=plot,plot2=plot2))
-}
