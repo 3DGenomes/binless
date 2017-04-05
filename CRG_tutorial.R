9914c5708c48fd899082ab7edc2064fce90e96ce
Yannick Spill <yannick.spill@crg.eu>
2017-04-06 00:33:39 +0200

    generate matrices for tutorial

--- CRG_tutorial.R
+++ CRG_tutorial.R
@@ -1,41 +1,43 @@
 library(data.table)
+library(scales)
+library(ggplot2)
+sfg=scale_fill_gradient(high=muted("red"), low="white", na.value = "white")
+sfg2=scale_fill_gradient2(high=muted("red"), low=muted("blue"), na.value = "white")
 
-load("data/rao_HiCall_FOXP1_1.3M_bpk30_csnorm_optimized.RData")
-resolution=20000
 
-mat=get_interactions(cs, type="interactions", resolution=resolution, group="all", threshold=0.95, ref="expected")
-mat=mat[,.(name,bin1,begin1,end1,bin2,begin2,end2,ncounts,observed,expected,expected.sd,
-           normalized,normalized.sd,signal,signal.sd,is.significant,direction)]
+resolution=10000 #try 10k, 20k and 50k
+load(paste0("data/rao_HiCall_FOXP1_1.3M_",resolution/1000,"k_signal.RData"))
+
+
 
 #observed data and expected counts
 ggplot(mat)+geom_raster(aes(begin1,begin2,fill=log(observed)))+
-  geom_raster(aes(begin2,begin1,fill=log(expected)))+facet_wrap(~name)+
-  scale_fill_gradient(high="red", low="white", na.value = "white")
+  geom_raster(aes(begin2,begin1,fill=log(expected)))+sfg+facet_wrap(~ name)
 
 #normalized matrix
 ggplot(mat)+geom_raster(aes(begin1,begin2,fill=pmin(normalized,5)))+
-  geom_raster(aes(begin2,begin1,fill=pmin(normalized,5)))+facet_wrap(~name)+
-  scale_fill_gradient(high="red", low="white", na.value = "white")
+  geom_raster(aes(begin2,begin1,fill=pmin(normalized,5)))+facet_wrap(~name)+sfg
 
 #normalized matrix with significant interactions 
 ggplot(mat)+geom_raster(aes(begin1,begin2,fill=pmin(normalized,5)))+
-  geom_raster(aes(begin2,begin1,fill=pmin(normalized,5)))+facet_wrap(~name)+
-  geom_point(aes(begin1,begin2,colour=direction),data=mat[is.significant==T&direction=="enriched"])+
-  scale_fill_gradient(high="red", low="white", na.value = "white")
+  geom_raster(aes(begin2,begin1,fill=pmin(normalized,5)))+facet_wrap(~name)+sfg+
+  geom_point(aes(begin1,begin2,colour=signal.direction),data=mat[signal.is.significant==T&signal.direction=="enriched"])
 
-#signal matrix with significant interactions 
-ggplot(mat)+geom_raster(aes(begin1,begin2,fill=signal))+
-  geom_raster(aes(begin2,begin1,fill=signal))+facet_wrap(~name)+
-  geom_point(aes(begin1,begin2,colour=direction),data=mat[is.significant==T&direction=="enriched"])+
-  scale_fill_gradient(high="red", low="white", na.value = "white")
+#binned signal with significant differences 
+ggplot(mat[!is.na(binned.difference)])+geom_raster(aes(begin1,begin2,fill=log(binned.difference)))+
+  facet_wrap(~name)+sfg2
+ggplot(mat[!is.na(binned.difference)])+geom_raster(aes(begin1,begin2,fill=log(binned.difference)))+
+  geom_raster(aes(begin2,begin1,fill=log(binned.difference)))+facet_wrap(~name)+sfg2+
+  geom_point(aes(begin1,begin2,colour=difference.direction),data=mat[difference.is.significant==T])
+
 
 #binless signal
-mat=get_interactions(cs, type="binteractions", resolution=resolution, group="all", threshold=-1, ref="expected")
-ggplot(mat)+geom_raster(aes(begin1,begin2,fill=value))+
-  geom_raster(aes(begin2,begin1,fill=value))+
-  facet_wrap(~name)+scale_fill_gradient(high="red", low="white", na.value = "white")
+ggplot(mat)+geom_raster(aes(begin1,begin2,fill=binless.signal))+
+  geom_raster(aes(begin2,begin1,fill=binless.signal))+
+  facet_wrap(~name)+sfg
 
-#binless patches
-ggplot(mat)+geom_raster(aes(begin1,begin2,fill=factor(patchno)))+
-  geom_raster(aes(begin2,begin1,fill=factor(patchno)))+
-  facet_wrap(~name)+guides(fill=F)+scale_colour_brewer(type="seq")
+#binless differences
+ggplot(mat[!is.na(binless.difference)])+facet_wrap(~name)+sfg+
+  geom_raster(aes(begin1,begin2,fill=binless.difference))+
+  geom_raster(aes(begin2,begin1,fill=binless.difference))
+  
