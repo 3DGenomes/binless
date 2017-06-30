8ba3197bb8789843e31163e5103027236a93e084
Yannick Spill <yannick.spill@crg.eu>
2017-06-30 09:59:28 +0200

    start using CV as criterion
    bug: does not respect positivity constraint

--- csnorm/src/optimize_lambda1_eCprime.cpp
+++ csnorm/src/optimize_lambda1_eCprime.cpp
@@ -118,6 +118,114 @@ NumericVector obj_lambda1_eCprime_BIC::get(double val, std::string msg) const {
                                  _["BIC"]=BIC, _["dof"]=dof, _["UB"]=UB, _["LB"]=LB);
 }
 
+
+obj_lambda1_eCprime_CV::obj_lambda1_eCprime_CV(double minval, double maxval,
+                                                 double tol_val,
+                                                 bool constrained, IntegerVector patchno, NumericVector forbidden_vals,
+                                                 NumericVector value, NumericVector weight, NumericVector valuehat,
+                                                 NumericVector ncounts, double lambda2) : minval_(minval), valrange_(maxval-minval),
+                                                 tol_val_(tol_val), lsnc_(log(sum(ncounts))), lambda2_(lambda2), constrained_(constrained),
+                                                 patchno_(patchno), forbidden_vals_(forbidden_vals),
+                                                 value_(value), weight_(weight), valuehat_(valuehat) {}
+
+double obj_lambda1_eCprime_CV::operator()(double val) const {
+  //return get(val, "opt")["CV"];
+  return get(val)["CV"];
+}
+
+//take val as a starting point for optimization of UB and LB at constant dof
+NumericVector obj_lambda1_eCprime_CV::get(double val, std::string msg) const {
+  //split data in two groups
+  LogicalVector grp1 = value_ <= val;
+  double b1,b2, xk, xkp1;
+  double a,b,UB,LB;
+  bool grp1only = is_true(all(grp1)), grp2only = is_true(all(!grp1));
+  double xmin = min(value_), xmax = max(value_);
+  
+  //compute UB and LB
+  if ((!grp1only) && (!grp2only)) {//non-degenerate case
+    NumericVector w1 = weight_[grp1];
+    NumericVector betahat1 = valuehat_[grp1];
+    NumericVector w2 = weight_[!grp1];
+    NumericVector betahat2 = valuehat_[!grp1];
+    NumericVector x1 = value_[grp1];
+    NumericVector x2 = value_[!grp1];
+    b1 = sum(w1*betahat1)/sum(w1);
+    b2 = sum(w2*(x2-betahat2))/sum(w2);
+    xk = max(x1);
+    xkp1 = min(x2);
+    //compute unconstrained minimum for UB and LB
+    a = b1 + b2;
+    b = b1 - b2;
+    //compute optimal UB and LB
+    UB = std::max(std::min(xkp1, a), xk);
+    LB = std::min(b, xmin);
+    
+  } else if (grp1only) {//grp2 is empty, UB > xmax
+    NumericVector w1 = weight_[grp1];
+    NumericVector betahat1 = valuehat_[grp1];
+    b1 = sum(w1*betahat1)/sum(w1);
+    b2 = 0;
+    xk = xmax;
+    xkp1 = std::numeric_limits<double>::infinity();
+    //compute unconstrained minimum for UB and LB
+    a = b1 + b2;
+    b = b1 - b2;
+    //compute optimal UB and LB
+    if (b1 >= (xmin+xmax)/2.) {
+      LB = xmin; //or any value < xmin
+      UB = 2*b1 - LB;
+    } else {
+      UB = xmax; //or any value > xmax
+      LB = 2*b1 - UB;
+    }
+    
+  } else {//grp1 is empty, UB <= xmin
+    NumericVector w2 = weight_[!grp1];
+    NumericVector betahat2 = valuehat_[!grp1];
+    NumericVector x2 = value_[!grp1];
+    b1 = 0;
+    b2 = sum(w2*(x2-betahat2))/sum(w2);
+    xk = -std::numeric_limits<double>::infinity();
+    xkp1 = xmin;
+    //compute unconstrained minimum for UB and LB
+    a = b1 + b2;
+    b = b1 - b2;
+    //compute optimal UB and LB
+    UB = xmin;
+    if (b2<-tol_val_) Rcout << " warning: b2= " << b2 << " is negative, setting UB=LB" << std::endl;
+    LB = UB - 2*(std::max(b2,0.)+tol_val_);
+  }
+  
+  //compute eCprime, lambda1
+  double eCprime = (UB+LB)/2;
+  double lambda1 = (UB-LB)/2;
+  /*Rcout << "EVAL at val= " << val << " LB= " << LB << " UB= " << UB << " b1= " << b1 << " b2= " << b2
+          << " xmin= " << xmin << " xk= " << xk << " xkp1= " << xkp1 << " a= " << a << " b= " << b << std::endl; */
+  //check if solution is feasible
+  if (constrained_) {
+    if ( is_true(any( (forbidden_vals_>UB+tol_val_/2) | (forbidden_vals_<LB-tol_val_/2) )) ) {
+      if (!msg.empty()) Rcout << " OBJ " << msg << " forbidden lambda2= " << lambda2_ << " lambda1= " << lambda1
+                              << " eCprime= " << eCprime << " CV= Inf dof= NA" << std::endl;
+      return NumericVector::create(_["eCprime"]=eCprime, _["lambda1"]=lambda1,
+                                   _["BIC"]=std::numeric_limits<double>::max(), _["dof"]=NumericVector::get_na(),
+                                   _["UB"]=UB, _["LB"]=LB);
+    }
+  }
+  //compute dof and CV
+  std::vector<double> value_r = as<std::vector<double> >(value_);
+  NumericVector soft = wrap(soft_threshold(value_r, eCprime, lambda1));
+  IntegerVector selected = patchno_[abs(soft)>tol_val_/2];
+  const int dof = unique(selected).size();
+  const double CV = mean(weight_ * SQUARE(valuehat_ - (soft + eCprime)));
+  if (!msg.empty()) Rcout << " OBJ " << msg << " ok lambda2= " << lambda2_ << " lambda1= " << lambda1
+                          << " eCprime= " << eCprime << " CV= " << CV  << " dof= " << dof 
+                          << " UB= " << UB  << " LB= " << LB << std::endl;
+  return NumericVector::create(_["eCprime"]=eCprime, _["lambda1"]=lambda1,
+                               _["BIC"]=CV, _["dof"]=dof, _["UB"]=UB, _["LB"]=LB);
+}
+
+
 NumericVector cpp_optimize_lambda1_eCprime(const DataFrame mat, int nbins,
         double tol_val, bool constrained,
         double lambda1_min, int refine_num, double lambda2) {
@@ -143,13 +251,13 @@ NumericVector cpp_optimize_lambda1_eCprime(const DataFrame mat, int nbins,
         lmin = std::max(lmin, (max(forbidden_vals)-minval)/2);
     }
     //create functor
-    obj_lambda1_eCprime_BIC obj(minval, maxval, tol_val, constrained, patchno,
+    /*obj_lambda1_eCprime_BIC obj(minval, maxval, tol_val, constrained, patchno,
                             forbidden_vals,
-                            beta, weight, phihat, ncounts, lambda2);
-    /*NumericVector beta_cv = mat["beta_cv"];
-      obj_lambda1_eCprime_CV obj(minval, maxval, tol_val, constrained, patchno,
-                                forbidden_vals,
-                                beta_cv, weight, phihat, ncounts, lambda2);*/
+                            beta, weight, phihat, ncounts, lambda2);*/
+    NumericVector beta_cv = mat["beta_cv"];
+    obj_lambda1_eCprime_CV obj(minval, maxval, tol_val, constrained, patchno,
+                               forbidden_vals,
+                               beta_cv, weight, phihat, ncounts, lambda2);
     //for (int i=0; i<forbidden_vals.size(); ++i) Rcout << "fv[ " << i << " ]= "<< forbidden_vals[i] << std::endl;
     //loop over patch values
     std::clock_t c_in1 = std::clock();
