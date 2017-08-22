05191d468314eb41b43104713ec5d4b97443e35e
Yannick Spill <yannick.spill@crg.eu>
2017-08-22 18:04:59 +0200

    finish IRLSEstimator implementation

--- csnorm/src/IRLSEstimator.hpp
+++ csnorm/src/IRLSEstimator.hpp
@@ -1,6 +1,7 @@
 #ifndef IRLS_ESTIMATOR_HPP
 #define IRLS_ESTIMATOR_HPP
 
+#include <Rcpp.h>
 #include <vector>
 
 #include "util.hpp"
@@ -15,10 +16,10 @@ public:
     //initialize the problem with a triangle grid with nrows
     //requesting precision to be below a given convergence criterion
     //final beta value will be clamped if clamp>0
-    IRLSEstimator(unsigned nrows, double converge)
-    : GaussianEstimator(nrows, converge), WeightsUpdater(),
-      nouter_(Settings<IRLSEstimator<Estimator, WeightsUpdater> >::get_nouter()),
-      converge_(converge), counter_(0) {}
+    IRLSEstimator(unsigned nrows, double converge, unsigned nouter, double dispersion,
+                  const Rcpp::DataFrame& cts, const Rcpp::List& outliers)
+    : GaussianEstimator(nrows, converge), WeightsUpdater(nrows, dispersion, cts, outliers),
+      nouter_(nouter), converge_(converge), counter_(0) {}
     
     //run the optimization on the given data. The objective is
     // sum_i w_i(y_i-beta_i)^2 + lambda2 * sum_ij |beta_i-beta_j|
@@ -26,20 +27,30 @@ public:
     // and updating beta with the chosen fused lasso implementation in GaussianEstimator.
     // Initial state will be computed from beta_init
     void optimize(const std::vector<double>& beta_init, double lambda2) {
-    
         beta_ = beta_init;
-        
-        for (counter_ = 0, std::vector<double> beta_old = beta_;
-             counter_ <= nouter_ && ( counter_ == 0 || get_precision(beta_,beta_old) > converge_ );
-             ++counter_, beta_old = beta_) {
-            //update weights
-            WeightsUpdater::update(beta_);
-            auto y = WeightsUpdater::get_y();
-            auto w = WeightsUpdater::get_w();
-            //estimate beta
-            GaussianEstimator::optimize(y, beta_, w, lambda2);
-            beta_ = GaussianEstimator::get();
-        }
+        counter_ = 0;
+        double precision = converge_+1;
+        std::vector<double> beta_old = beta_;
+        /*Rcpp::Rcout << " Perf iteration: start with lam2= " << lambda2 << " alpha= "
+                    << GaussianEstimator::get_alpha() << " phi[0]= " << beta_[0] << "\n";*/
+        do {
+          //update weights
+          WeightsUpdater::update(beta_);
+          auto y = WeightsUpdater::get_y();
+          auto w = WeightsUpdater::get_w();
+          //estimate beta
+          GaussianEstimator::optimize(y, beta_, w, lambda2);
+          beta_ = GaussianEstimator::get();
+          //update counters and compute precision
+          precision = get_precision(beta_,beta_old);
+          ++counter_;
+          beta_old = beta_;
+          /*Rcpp::Rcout << " Iteration " << counter_ << " with lam2= " << lambda2 << " alpha= "
+            << GaussianEstimator::get_alpha() << " reached maxval= " << precision
+            << " after " << GaussianEstimator::get_ninner() << " steps " << " phi[0]= " << beta_[0] << "\n";*/
+        } while (counter_ <= nouter_ && precision > converge_ );
+        /*Rcpp::Rcout << " Perf iteration: end with lam2= " << lambda2 << " alpha= "
+        << GaussianEstimator::get_alpha() << " phi[0]= " << beta_[0] << "\n";*/
         
     }
     
@@ -53,7 +64,7 @@ public:
     
 private:
     double get_precision(const std::vector<double>& beta, const std::vector<double>& beta_old) const {
-        maxval = std::abs(beta[0]-beta_old[0]);
+        double maxval = std::abs(beta[0]-beta_old[0]);
         const unsigned N = beta.size();
         for (unsigned i=1; i < N; ++i) {
             maxval = std::max(std::abs(beta[i]-beta_old[i]), maxval);
--- csnorm/src/Settings.hpp
+++ csnorm/src/Settings.hpp
@@ -2,19 +2,33 @@
 #define SETTINGS_HPP
 
 //class to store all settings used in the c++ side 
+//to add settings for a new class, forward-declare the class
+//and then specialize the Settings template on it
 template<typename T> struct Settings {};
 
 //settings for GFLLibrary
 class GFLLibrary;
 template<> struct Settings<GFLLibrary> {
+    //inflation factor for adamts step update
     static const double get_inflate() { return 2.; }
+    //maximum number of adamts steps
     static const int get_ninner() { return 100000; }
 };
 
 //settings for FusedLassoGaussianEstimator
 template<typename Library> class FusedLassoGaussianEstimator;
 template<typename Library> struct Settings<FusedLassoGaussianEstimator<Library> > {
+    //at which value to clamp computed beta. Negative to turn off
     static const double get_clamp() { return 50; }
+};
+
+//settings for IRLSEstimator
+template<typename Estimator, typename WeightsUpdater>
+class IRLSEstimator;
+template<typename Estimator, typename WeightsUpdater>
+struct Settings<IRLSEstimator<Estimator, WeightsUpdater> > {
+    //maximum number of outer IRLS iterations (weight updates)
+    static const unsigned get_nouter() { return 500; }
 };
 
 #endif
