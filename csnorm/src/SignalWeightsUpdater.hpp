aa0987b028e2bc8177466bd9a373b630d44a89a4
Yannick Spill <yannick.spill@crg.eu>
2017-08-23 08:31:29 +0200

    SignalWeightsUpdater: move all code to header

--- csnorm/src/SignalWeightsUpdater.hpp
+++ csnorm/src/SignalWeightsUpdater.hpp
@@ -4,6 +4,8 @@
 #include <Rcpp.h>
 #include <vector>
 
+#include "cts_to_mat.hpp"
+
 //policy class that updates IRLS data and weights given counts and a new beta
 //This class takes care of the case of signal estimation
 class SignalWeightsUpdater {
@@ -13,7 +15,9 @@ public:
                          const Rcpp::DataFrame& cts, const Rcpp::List& outliers) :
        nrows_(nrows), dispersion_(dispersion), cts_(cts), outliers_(outliers) {}
     
-    void update(const std::vector<double>& beta);
+    void update(const std::vector<double>& beta) {
+        mat_ = cts_to_signal_mat(cts_, nrows_, dispersion_, beta, 0, outliers_);
+    }
     
     std::vector<double> get_y() const { return Rcpp::as<std::vector<double> >(mat_["phihat"]); }
     
