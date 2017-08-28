234800db9fcf737a31e706dfe1ad187bb092a8a7
Yannick Spill <yannick.spill@crg.eu>
2017-08-28 14:12:18 +0200

    same for optimize_lambda1_diff

--- csnorm/src/optimize_lambda1_diff.hpp
+++ csnorm/src/optimize_lambda1_diff.hpp
@@ -4,9 +4,22 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 #include <vector>
+#include <utility> //pair
+
+
+struct obj_lambda1_diff_base {
+    typedef double bounds_t;
+    obj_lambda1_diff_base(NumericVector value, NumericVector weight, NumericVector valuehat, double minUB);
+    //given an UB candidate (and implicit dof), find the adequate UB and LB
+    bounds_t optimize_bounds(double UB) const;
+    
+private:
+    NumericVector value_, absval_, weight_, valuehat_;
+    double minabsval_, maxabsval_, minUB_;
+};
 
 //objective functor to find lambda1 assuming eCprime=0, using BIC
-struct obj_lambda1_diff_BIC {
+struct obj_lambda1_diff_BIC : private obj_lambda1_diff_base {
   obj_lambda1_diff_BIC(double minUB, double tol_val,
                   IntegerVector patchno, NumericVector forbidden_vals,
                   NumericVector value, NumericVector weight, NumericVector valuehat,
@@ -23,7 +36,7 @@ struct obj_lambda1_diff_BIC {
 };
 
 //objective functor to find lambda1 assuming eCprime=0, using CV
-struct obj_lambda1_diff_CV {
+struct obj_lambda1_diff_CV : private obj_lambda1_diff_base {
     obj_lambda1_diff_CV(double minUB, double tol_val,
                 IntegerVector patchno, NumericVector forbidden_vals,
                 NumericVector value, NumericVector weight, NumericVector valuehat,
