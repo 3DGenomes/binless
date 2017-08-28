ffd55d55ca4c9be1b8ce96c1eda0f1b6636a3fda
Yannick Spill <yannick.spill@crg.eu>
2017-08-28 16:50:18 +0200

    put CV calculation for differences in common base class

--- csnorm/src/optimize_lambda1_diff.cpp
+++ csnorm/src/optimize_lambda1_diff.cpp
@@ -10,6 +10,50 @@ using namespace Rcpp;
 #include "graph_helpers.hpp" //get_patch_numbers
 
 
+
+
+Rcpp::NumericVector compute_CV_diff::evaluate(double UB) const {
+    //compute dof and CV
+    const double lambda1 = UB;
+    const double eCprime = 0;
+    std::vector<double> value_r = as<std::vector<double> >(value_);
+    std::vector<double> soft_r = soft_threshold(value_r, 0, lambda1);
+    NumericVector soft = wrap(soft_r);
+    std::vector<double> valuehat_r = as<std::vector<double> >(valuehat_);
+    std::vector<double> valuehat_ref_r = as<std::vector<double> >(valuehat_ref_);
+    std::vector<double> var, var_ref;
+    var.reserve(valuehat_r.size());
+    var_ref.reserve(valuehat_r.size());
+    for (int i=0; i<valuehat_r.size(); ++i) {
+        var.push_back(1/weight_(i));
+        var_ref.push_back(1/weight_ref_(i));
+    }
+    NumericVector phi_ref = wrap(compute_phi_ref(soft_r, valuehat_r, var, valuehat_ref_r, var_ref));
+    
+    IntegerVector selected = patchno_[abs(soft)>tol_val_/2];
+    const int dof = unique(selected).size();
+    
+    const NumericVector indiv_CV = weight_ref_ * SQUARE(valuehat_ref_ - phi_ref) + weight_*SQUARE(valuehat_-(phi_ref+soft));
+    NumericVector groupwise_CV, groupwise_weights;
+    const int ngroups=2;
+    for (int i=0; i<ngroups; ++i) {
+        groupwise_CV.push_back(sum(as<NumericVector>(indiv_CV[cv_grp_==i])));
+        groupwise_weights.push_back(sum(cv_grp_==i));
+    }
+    const double CV = sum(groupwise_weights*groupwise_CV)/sum(groupwise_weights);
+    const double CV_sd = std::sqrt(sum(groupwise_weights*SQUARE(groupwise_CV))/sum(groupwise_weights) - SQUARE(CV));
+    
+    /*Rcout << " OBJ " << msg << " ok lambda1= " << lambda1 << " eCprime= 0"
+        << " CV= " << CV  << " dof= " << dof
+        << " UB= " << lambda1 << " LB= " << -lambda1 << std::endl;*/
+    return NumericVector::create(_["eCprime"]=eCprime, _["lambda1"]=lambda1, _["BIC"]=CV, _["BIC.sd"]=CV_sd,
+                                 _["dof"]=dof, _["UB"]=UB, _["LB"]=-UB);
+}
+
+
+
+
+
 obj_lambda1_diff_BIC::obj_lambda1_diff_BIC(double minUB, double tol_val,
                                  IntegerVector patchno, NumericVector forbidden_vals,
                                  NumericVector value, NumericVector weight, NumericVector valuehat,
@@ -55,6 +99,7 @@ obj_lambda1_diff_CV::obj_lambda1_diff_CV(double minUB, double tol_val,
                          NumericVector weight_ref, NumericVector valuehat_ref,
                          NumericVector ncounts, IntegerVector cv_grp) :
     obj_lambda1_base(value, weight, valuehat, minUB),
+    compute_CV_diff(tol_val, value, weight, valuehat, weight_ref, valuehat_ref, patchno, cv_grp),
     minUB_(minUB), tol_val_(tol_val), lsnc_(log(sum(ncounts))), forbidden_vals_(forbidden_vals),
     patchno_(patchno), value_(value), weight_(weight), valuehat_(valuehat), weight_ref_(weight_ref),
     valuehat_ref_(valuehat_ref), cv_grp_(cv_grp) {}
@@ -81,38 +126,7 @@ NumericVector obj_lambda1_diff_CV::get(double val, std::string msg) const {
                                    _["dof"]=NumericVector::get_na(),
                                    _["UB"]=lambda1, _["LB"]=-lambda1);
   }
-    std::vector<double> value_r = as<std::vector<double> >(value_);
-    std::vector<double> soft_r = soft_threshold(value_r, 0, lambda1);
-    NumericVector soft = wrap(soft_r);
-    std::vector<double> valuehat_r = as<std::vector<double> >(valuehat_);
-    std::vector<double> valuehat_ref_r = as<std::vector<double> >(valuehat_ref_);
-    std::vector<double> var, var_ref;
-    var.reserve(valuehat_r.size());
-    var_ref.reserve(valuehat_r.size());
-    for (int i=0; i<valuehat_r.size(); ++i) {
-      var.push_back(1/weight_(i));
-      var_ref.push_back(1/weight_ref_(i));
-    }
-    NumericVector phi_ref = wrap(compute_phi_ref(soft_r, valuehat_r, var, valuehat_ref_r, var_ref));
-    
-    IntegerVector selected = patchno_[abs(soft)>tol_val_/2];
-    const int dof = unique(selected).size();
-    
-    const NumericVector indiv_CV = weight_ref_ * SQUARE(valuehat_ref_ - phi_ref) + weight_*SQUARE(valuehat_-(phi_ref+soft));
-    NumericVector groupwise_CV, groupwise_weights;
-    const int ngroups=2;
-    for (int i=0; i<ngroups; ++i) {
-      groupwise_CV.push_back(sum(as<NumericVector>(indiv_CV[cv_grp_==i])));
-      groupwise_weights.push_back(sum(cv_grp_==i));
-    }
-    const double CV = sum(groupwise_weights*groupwise_CV)/sum(groupwise_weights);
-    const double CV_sd = std::sqrt(sum(groupwise_weights*SQUARE(groupwise_CV))/sum(groupwise_weights) - SQUARE(CV));
-    
-    if (!msg.empty()) Rcout << " OBJ " << msg << " ok lambda1= " << lambda1 << " eCprime= 0"
-                            << " CV= " << CV  << " dof= " << dof
-                            << " UB= " << lambda1 << " LB= " << -lambda1 << std::endl;
-    return NumericVector::create(_["eCprime"]=0, _["lambda1"]=lambda1, _["BIC"]=CV, _["BIC.sd"]=CV_sd,
-                                 _["dof"]=dof, _["UB"]=UB, _["LB"]=-UB);
+    return evaluate(UB);
 }
 
 NumericVector cpp_optimize_lambda1_diff(const DataFrame mat, int nbins,
--- csnorm/src/optimize_lambda1_diff.hpp
+++ csnorm/src/optimize_lambda1_diff.hpp
@@ -8,6 +8,19 @@ using namespace Rcpp;
 
 #include "optimize_lambda1.hpp" //obj_lambda1_base
 
+struct compute_CV_diff {
+    compute_CV_diff(double tol_val, const NumericVector& value, const NumericVector& weight, const NumericVector& valuehat,
+                    const NumericVector& weight_ref, const NumericVector& valuehat_ref,
+                    const IntegerVector& patchno, const IntegerVector& cv_grp) :
+    tol_val_(tol_val), value_(value), weight_(weight), valuehat_(valuehat), weight_ref_(weight_ref),
+    valuehat_ref_(valuehat_ref), patchno_(patchno), cv_grp_(cv_grp) {}
+    NumericVector evaluate(double UB) const;
+private:
+    double tol_val_;
+    NumericVector value_, weight_, valuehat_, weight_ref_, valuehat_ref_;
+    IntegerVector patchno_, cv_grp_;
+};
+
 //objective functor to find lambda1 assuming eCprime=0, using BIC
 struct obj_lambda1_diff_BIC : private obj_lambda1_base {
   obj_lambda1_diff_BIC(double minUB, double tol_val,
@@ -26,7 +39,7 @@ struct obj_lambda1_diff_BIC : private obj_lambda1_base {
 };
 
 //objective functor to find lambda1 assuming eCprime=0, using CV
-struct obj_lambda1_diff_CV : private obj_lambda1_base {
+struct obj_lambda1_diff_CV : private obj_lambda1_base, private compute_CV_diff {
     obj_lambda1_diff_CV(double minUB, double tol_val,
                 IntegerVector patchno, NumericVector forbidden_vals,
                 NumericVector value, NumericVector weight, NumericVector valuehat,
