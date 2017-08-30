016d92513c9c096c4584d92655dabb987c56602b
Yannick Spill <yannick.spill@crg.eu>
2017-08-30 09:14:53 +0200

    AnySignZeroOffsetBounds: fix case where weight is zero

--- csnorm/src/Bounds.cpp
+++ csnorm/src/Bounds.cpp
@@ -10,14 +10,14 @@ PositiveEstimatedOffsetBounds::bounds_t PositiveEstimatedOffsetBounds::optimize_
     LogicalVector grp1 = value_ <= val;
     NumericVector w1 = weight_[grp1];
     NumericVector w2 = weight_[!grp1];
-    bool grp1only = w2.size() == 0 || sum(w2) == 0;
-    bool grp2only = w1.size() == 0 || sum(w1) == 0;
+    bool grp2empty = w2.size() == 0 || sum(w2) == 0;
+    bool grp1empty = w1.size() == 0 || sum(w1) == 0;
     double b1,b2, xk, xkp1;
     double a,b,UB,LB;
     double xmin = min(value_), xmax = max(value_); //here, we assume xmin >= minval_
   
   //compute UB and LB
-  if ((!grp1only) && (!grp2only)) {//non-degenerate case
+  if ((!grp2empty) && (!grp1empty)) {//non-degenerate case
       NumericVector betahat1 = valuehat_[grp1];
       NumericVector betahat2 = valuehat_[!grp1];
       NumericVector x1 = value_[grp1];
@@ -32,7 +32,7 @@ PositiveEstimatedOffsetBounds::bounds_t PositiveEstimatedOffsetBounds::optimize_
       //compute optimal UB and LB
       UB = std::max(std::min(xkp1, a), xk);
       LB = std::min(b, minval_);
-  } else if (grp1only) {//grp2 is empty, UB > xmax
+  } else if (grp2empty) {//grp2 is empty, UB > xmax
       NumericVector w1 = weight_;
       NumericVector betahat1 = valuehat_;
       b1 = sum(w1*betahat1)/sum(w1);
@@ -51,7 +51,7 @@ PositiveEstimatedOffsetBounds::bounds_t PositiveEstimatedOffsetBounds::optimize_
           LB = 2*b1 - UB;
       }
   } else {//grp1 is empty, UB <= xmin
-      if (!grp2only) Rcout << "This should not have happened!\n";
+      if (!grp1empty) Rcout << "This should not have happened!\n";
       NumericVector w2 = weight_;
       NumericVector betahat2 = valuehat_;
       NumericVector x2 = value_;
@@ -79,6 +79,10 @@ AnySignZeroOffsetBounds::bounds_t AnySignZeroOffsetBounds::optimize_bounds(doubl
     //split data in two groups and determine constraint values
     //Rcout << "  GET at " << val << " maxabsval_= " << maxabsval_ << std::endl;
     LogicalVector grp1 = value_ > val, grp2 = value_ < -val;
+    NumericVector w1 = weight_[grp1];
+    NumericVector w2 = weight_[grp2];
+    bool grp1empty = w1.size() == 0 || sum(w1) == 0;
+    bool grp2empty = w2.size() == 0 || sum(w2) == 0;
     double xk,xkp1;
     if (val>maxabsval_) {
         xk=maxabsval_;
@@ -93,29 +97,24 @@ AnySignZeroOffsetBounds::bounds_t AnySignZeroOffsetBounds::optimize_bounds(doubl
     //Rcout << "  xk= " << xk << " xkp1= " << xkp1 << std::endl;
     //determine unconstrained UB
     double UB;
-    bool grp1empty = is_true(all(!grp1)), grp2empty = is_true(all(!grp2));
     //Rcout << "  grp1empty= " << grp1empty << " grp2empty= " << grp2empty << std::endl;
     if (grp1empty && grp2empty) {
         //UB larger than any value
         UB=maxabsval_;
     } else if (grp1empty) {
         //UB larger than any positive value
-        NumericVector w2 = weight_[grp2];
         NumericVector betahat2 = valuehat_[grp2];
         NumericVector x2 = value_[grp2];
         UB = -sum(w2*(x2-betahat2))/sum(w2);
     } else if (grp2empty) {
-        NumericVector w1 = weight_[grp1];
         NumericVector betahat1 = valuehat_[grp1];
         NumericVector x1 = value_[grp1];
         UB = sum(w1*(x1-betahat1))/sum(w1);
     } else {
-        NumericVector w1 = weight_[grp1];
         NumericVector betahat1 = valuehat_[grp1];
         NumericVector x1 = value_[grp1];
         double sw1 = sum(w1);
         double b1 = sum(w1*(x1-betahat1))/sw1;
-        NumericVector w2 = weight_[grp2];
         NumericVector betahat2 = valuehat_[grp2];
         NumericVector x2 = value_[grp2];
         double sw2 = sum(w2);
