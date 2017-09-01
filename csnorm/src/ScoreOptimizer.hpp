d2bb4976a2fcf5da6cc9e286e42768d641c793ed
Yannick Spill <yannick.spill@crg.eu>
2017-09-01 11:00:08 +0200

    do not template on k

--- csnorm/src/ScoreOptimizer.hpp
+++ csnorm/src/ScoreOptimizer.hpp
@@ -13,25 +13,23 @@ public:
         if (values.size()==0) Rcpp::stop("Found 0 values!\n");
         //find minimum
         auto minval_it = std::min_element(values.cbegin(), values.cend(),
-                    [](const value_t& a, const value_t& b) {return Rcpp::is_true(a["BIC"]<b["BIC"]); } );
+                    [](const value_t& a, const value_t& b) {return Rcpp::as<double>(a["BIC"])<Rcpp::as<double>(b["BIC"]); } );
         //optionally, go upwards
-        return best = add_kSD<Score::k>(minval_it, values.cend());
+        return add_kSD(minval_it, values.cend());
     }
     
 private:
-    template<unsigned k> value_t add_kSD(const std::vector<value_t>::const_iterator minval_it,
-                                         const std::vector<value_t>::const_iterator minval_end) const {
+    value_t add_kSD(const std::vector<value_t>::const_iterator minval_it,
+                    const std::vector<value_t>::const_iterator minval_end) const {
         auto it = minval_it;
-        const double minscore = (*it)["BIC"] + k * (*it)["BIC.sd"];
-        do { ++it; } while (it != minval_end && Rcpp::as<double>((*it)["BIC"]) <= minscore );
-        if (it == minval_end) --it;
+        if (Score::k > 0) {
+          const double minscore = (*it)["BIC"] + Score::k * (*it)["BIC.sd"];
+          do { ++it; } while (it != minval_end && Rcpp::as<double>((*it)["BIC"]) <= minscore );
+          if (it == minval_end) --it;
+        }
         return *it;
     }
-    
-    template<> value_t add_kSD<0>(std::vector<value_t>::const_iterator minval_it,
-                                  const std::vector<value_t>::const_iterator) const {
-        return (*minval_it);
-    }
-    
 };
+
+
 #endif
