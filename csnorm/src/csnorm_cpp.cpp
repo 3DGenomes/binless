0348a68f4fcd72af94e0a611525f255da242c49c
Yannick Spill <yannick.spill@crg.eu>
2017-09-01 15:42:01 +0200

    do not expose cts_to_*_mat, which will soon change

--- csnorm/src/csnorm_cpp.cpp
+++ csnorm/src/csnorm_cpp.cpp
@@ -9,8 +9,6 @@ using namespace Rcpp;
 RCPP_MODULE(csnorm_cpp) {
     using namespace Rcpp ;
 
-    function("cts_to_signal_mat", &cts_to_signal_mat,
-             "documentation for cts_to_signal_mat ");
     function("wgfl_signal_perf_warm", &wgfl_signal_perf_warm,
              "documentation for wgfl_signal_perf_warm ");
     function("wgfl_signal_BIC", &wgfl_signal_BIC,
@@ -18,8 +16,6 @@ RCPP_MODULE(csnorm_cpp) {
     function("wgfl_signal_BIC_fixed", &wgfl_signal_BIC_fixed,
              "documentation for wgfl_signal_BIC_fixed ");
 
-    function("cts_to_diff_mat", &cts_to_diff_mat,
-             "documentation for cts_to_diff_mat ");
     function("wgfl_diff_perf_warm", &wgfl_diff_perf_warm,
              "documentation for wgfl_diff_perf_warm ");
     function("wgfl_diff_BIC", &wgfl_diff_BIC, "documentation for wgfl_diff_BIC ");
