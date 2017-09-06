6658ca0f45843c2d053ca0946e660834f37b2308
Yannick Spill <yannick.spill@crg.eu>
2017-09-06 16:14:51 +0200

    make BinnedData a public base class

--- csnorm/src/BinnedData.hpp
+++ csnorm/src/BinnedData.hpp
@@ -3,7 +3,7 @@
 
 #include <Rcpp.h>
 
-//generic binned data: input is y and weight, estimate is beta etc.
+//generic binned data: input is betahat and weight, estimate is beta etc.
 class BinnedData {
 public:
     Rcpp::IntegerVector get_bin1() const { return bin1_; }
@@ -18,8 +18,8 @@ public:
     Rcpp::NumericVector get_weight() const { return weight_; }
     void set_weight(const Rcpp::NumericVector& weight) { weight_ = weight; }
     
-    Rcpp::NumericVector get_y() const { return y_; }
-    void set_y(const Rcpp::NumericVector& y) { y_ = y; }
+    Rcpp::NumericVector get_betahat() const { return betahat_; }
+    void set_betahat(const Rcpp::NumericVector& betahat) { betahat_ = betahat; }
     
     Rcpp::NumericVector get_ncounts() const { return ncounts_; }
     void set_ncounts(const Rcpp::NumericVector& ncounts) { ncounts_ = ncounts; }
@@ -35,72 +35,31 @@ public:
     
 private:
     Rcpp::IntegerVector bin1_, bin2_;
-    Rcpp::NumericVector beta_, weight_, y_, ncounts_;
+    Rcpp::NumericVector beta_, weight_, betahat_, ncounts_;
     Rcpp::IntegerVector patchno_, diag_idx_, diag_grp_;
 };
 
 //signal binned data: input is phihat and weight, estimate is beta, phi etc.
-class SignalBinnedData : private BinnedData {
+class SignalBinnedData : public BinnedData {
 public:
-    using BinnedData::get_bin1;
-    using BinnedData::set_bin1;
-    
-    using BinnedData::get_bin2;
-    using BinnedData::set_bin2;
-    
+
     Rcpp::NumericVector get_beta_phi() const { return get_beta(); }
     void set_beta_phi(const Rcpp::NumericVector& beta_phi) { set_beta(beta_phi); }
     
-    using BinnedData::get_weight;
-    using BinnedData::set_weight;
-    
-    Rcpp::NumericVector get_phihat() const { return get_y(); }
-    void set_phihat(const Rcpp::NumericVector& phihat) { set_y(phihat); }
-    
-    using BinnedData::get_ncounts;
-    using BinnedData::set_ncounts;
-    
-    using BinnedData::get_patchno;
-    using BinnedData::set_patchno;
-    
-    using BinnedData::get_diag_idx;
-    using BinnedData::set_diag_idx;
-    
-    using BinnedData::get_diag_grp;
-    using BinnedData::set_diag_grp;
+    Rcpp::NumericVector get_phihat() const { return get_betahat(); }
+    void set_phihat(const Rcpp::NumericVector& phihat) { set_betahat(phihat); }
 };
 
 //signal binned data: input is phihat and weight, estimate is beta, phi etc.
-class DifferenceBinnedData : private BinnedData {
+class DifferenceBinnedData : public BinnedData {
 public:
-    using BinnedData::get_bin1;
-    using BinnedData::set_bin1;
-    
-    using BinnedData::get_bin2;
-    using BinnedData::set_bin2;
-    
     Rcpp::NumericVector get_beta_delta() const { return get_beta(); }
     void set_beta_delta(const Rcpp::NumericVector& beta_delta) { set_beta(beta_delta); }
     
-    using BinnedData::get_weight;
-    using BinnedData::set_weight;
-    
-    Rcpp::NumericVector get_deltahat() const { return get_y(); }
-    void set_deltahat(const Rcpp::NumericVector& deltahat) { set_y(deltahat); }
+    Rcpp::NumericVector get_deltahat() const { return get_betahat(); }
+    void set_deltahat(const Rcpp::NumericVector& deltahat) { set_betahat(deltahat); }
     
     Rcpp::NumericVector get_phihat() const { return get_phihat_ref() + get_deltahat(); }
-    
-    using BinnedData::get_ncounts;
-    using BinnedData::set_ncounts;
-    
-    using BinnedData::get_patchno;
-    using BinnedData::set_patchno;
-    
-    using BinnedData::get_diag_idx;
-    using BinnedData::set_diag_idx;
-    
-    using BinnedData::get_diag_grp;
-    using BinnedData::set_diag_grp;
     
     Rcpp::NumericVector get_weight_ref() const { return weight_ref_; }
     void set_weight_ref(const Rcpp::NumericVector& weight_ref) { weight_ref_ = weight_ref; }
