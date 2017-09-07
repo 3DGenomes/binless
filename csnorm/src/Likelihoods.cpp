b383db1de1f3f005d3461035a90340c18e8e8190
Yannick Spill <yannick.spill@crg.eu>
2017-09-07 09:41:20 +0200

    store reference to Data object

--- csnorm/src/Likelihoods.cpp
+++ csnorm/src/Likelihoods.cpp
@@ -10,7 +10,7 @@ Rcpp::NumericVector SignalLikelihood::get_chi_square(double LB, double UB) const
         std::vector<double> beta_phi_r = as<std::vector<double> >(beta_phi_);
         Rcpp::NumericVector phi = wrap(soft_threshold(beta_phi_r, eCprime, lambda1));
         
-        const Rcpp::NumericVector chisq = weight_ * SQUARE(phihat_ - (phi + eCprime));
+        const Rcpp::NumericVector chisq = binned_.get_weight() * SQUARE(binned_.get_phihat() - (phi + eCprime));
         return chisq;
     }
 
@@ -20,20 +20,23 @@ Rcpp::NumericVector DifferenceLikelihood::get_chi_square(double LB, double UB) c
     std::vector<double> beta_delta_r = as<std::vector<double> >(beta_delta_);
     std::vector<double> delta_r = soft_threshold(beta_delta_r, eCprime, lambda1);
     Rcpp::NumericVector delta = wrap(delta_r);
-    std::vector<double> deltahat_r = as<std::vector<double> >(deltahat_);
-    std::vector<double> phihat_ref_r = as<std::vector<double> >(phihat_ref_);
+    std::vector<double> deltahat_r = as<std::vector<double> >(binned_.get_deltahat());
+    Rcpp::NumericVector phihat_ref = binned_.get_phihat_ref();
+    std::vector<double> phihat_ref_r = as<std::vector<double> >(phihat_ref);
+    Rcpp::NumericVector weight = binned_.get_weight();
+    Rcpp::NumericVector weight_ref = binned_.get_weight_ref();
     std::vector<double> phihat_r, var, var_ref;
     phihat_r.reserve(deltahat_r.size());
     var.reserve(deltahat_r.size());
     var_ref.reserve(deltahat_r.size());
     for (int i=0; i<deltahat_r.size(); ++i) {
-        phihat_r.push_back(phihat_ref_(i) + deltahat_(i));
-        var.push_back(1/weight_(i));
-        var_ref.push_back(1/weight_ref_(i));
+        phihat_r.push_back(phihat_ref_r[i] + deltahat_r[i]);
+        var.push_back(1/weight(i));
+        var_ref.push_back(1/weight_ref(i));
     }
     Rcpp::NumericVector phi_ref = wrap(compute_phi_ref(delta_r, phihat_r, var, phihat_ref_r, var_ref));
     Rcpp::NumericVector phihat = wrap(phihat_r);
-    const Rcpp::NumericVector chisq = weight_ * SQUARE(phihat - (delta + eCprime + phi_ref))
-                                      + weight_ref_ * SQUARE(phihat_ref_ - phi_ref);
+    const Rcpp::NumericVector chisq = weight * SQUARE(phihat - (delta + eCprime + phi_ref))
+                                      + weight_ref * SQUARE(phihat_ref - phi_ref);
     return chisq;
 }
--- csnorm/src/Likelihoods.hpp
+++ csnorm/src/Likelihoods.hpp
@@ -8,26 +8,26 @@
 class SignalLikelihood {
 public:
     typedef Rcpp::NumericVector var_t;
-    SignalLikelihood(const SignalBinnedData& data, const var_t& beta_phi) :
-       beta_phi_(beta_phi), weight_(data.get_weight()), phihat_(data.get_phihat()) {}
+    SignalLikelihood(const SignalBinnedData& data, const var_t& beta_phi) : binned_(data), beta_phi_(beta_phi) {}
     
     Rcpp::NumericVector get_chi_square(double LB, double UB) const;
     
 private:
-    Rcpp::NumericVector beta_phi_, weight_, phihat_;
+    const SignalBinnedData& binned_;
+    const Rcpp::NumericVector beta_phi_;
 };
 
 class DifferenceLikelihood {
 public:
     typedef Rcpp::NumericVector var_t;
     DifferenceLikelihood(const DifferenceBinnedData& data, const var_t& beta_delta) :
-    beta_delta_(beta_delta), weight_(data.get_weight()), deltahat_(data.get_deltahat()),
-    weight_ref_(data.get_weight_ref()), phihat_ref_(data.get_phihat_ref()) {}
+    binned_(data), beta_delta_(beta_delta) {}
     
     Rcpp::NumericVector get_chi_square(double LB, double UB) const;
     
 private:
-    Rcpp::NumericVector beta_delta_, weight_, deltahat_, weight_ref_, phihat_ref_;
+    const DifferenceBinnedData& binned_;
+    const Rcpp::NumericVector beta_delta_;
 };
 
 #endif
