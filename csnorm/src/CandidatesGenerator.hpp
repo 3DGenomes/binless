fefed889cbc054d00ade1fca56faa0d2f92630ce
Yannick Spill <yannick.spill@crg.eu>
2017-09-28 12:30:36 +0200

    split minimum UB generation out of CandidatesGenerator

--- csnorm/src/CandidatesGenerator.hpp
+++ csnorm/src/CandidatesGenerator.hpp
@@ -6,14 +6,12 @@
 #include <algorithm>
 
 #include "CandidatesValues.hpp"
-#include "CandidatesUBMinDegen.hpp"
-#include "CandidatesUBMinOsi.hpp"
 #include "BinnedData.hpp"
-
-
+#include "minimum_UB.hpp"
 
 Rcpp::NumericVector get_patch_values(const Rcpp::NumericVector& value, const Rcpp::IntegerVector& patchno);
-Rcpp::NumericVector borders_from_values(int nbins, double tol_val, const BinnedDataCore& binned, const Rcpp::NumericVector& values);
+Rcpp::NumericVector borders_from_values(int nbins, double tol_val, const BinnedDataCore& binned,
+                                        const Rcpp::NumericVector& values);
 
 Rcpp::NumericVector cleanup_borders(const Rcpp::NumericVector& borders, double minUB);
 
@@ -25,28 +23,22 @@ Rcpp::NumericVector candidates_from_borders(const Rcpp::NumericVector& borders);
 template<typename Offset, //whether offset should be held at zero (ZeroOffset) or estimated (EstimatedOffset)
          typename Sign, //whether there is no constraint on the sign of the estimate (AnySign) or whether it must be positive (PositiveSign)
          typename Degeneracy> //whether to forbid certain values (ForbidDegeneracy) in order to avoid degeneracies in the model, or not (AllowDegeneracy)
-class CandidatesGenerator : private CandidatesValues<Offset,Sign>,
-                            private CandidatesUBMinDegen<Degeneracy>,
-                            private CandidatesUBMinOsi<Offset,Sign> {
+class CandidatesGenerator : private CandidatesValues<Offset,Sign> {
 public:
     
     CandidatesGenerator(int nbins, double tol_val, const BinnedDataCore& binned)
       : CandidatesValues<Offset,Sign>(binned),
-        CandidatesUBMinDegen<Degeneracy>(binned),
-        CandidatesUBMinOsi<Offset,Sign>(binned),
         UBcandidates_(generate(nbins, tol_val, binned)) {}
 
     Rcpp::NumericVector get_UB_candidates() const { return UBcandidates_; }
 
 private:
     Rcpp::NumericVector generate(int nbins, double tol_val, const BinnedDataCore& binned) const {
+        //get minimum admissible UB
+        double minUB = get_minimum_UB<Offset,Sign,Degeneracy>(binned);
         //get UB borders: values at which dof changes (including zero weight)
         Rcpp::NumericVector values = CandidatesValues<Offset,Sign>::get();
         Rcpp::NumericVector borders = borders_from_values(nbins,tol_val, binned, values);
-        //get minimum admissible UB
-        double minUBd = CandidatesUBMinDegen<Degeneracy>::get(values);
-        double minUBo = CandidatesUBMinOsi<Offset,Sign>::get();
-        double minUB = std::max(minUBd,minUBo);
         //cleanup the borders list, adding minUB as the lowest possible, expanding at the end
         borders = cleanup_borders(borders, minUB);
         //now generate candidates from these borders
@@ -54,7 +46,7 @@ private:
         Rcpp::NumericVector UBcandidates = candidates_from_borders(borders);
         return UBcandidates;
     }
-    
+   
     const Rcpp::NumericVector UBcandidates_;
 };
 
---
+++ csnorm/src/minimum_UB.hpp
@@ -0,0 +1,33 @@
+#ifndef MINIMUM_UB_HPP
+#define MINIMUM_UB_HPP
+
+#include <Rcpp.h>
+#include <vector>
+#include <algorithm>
+
+#include "CandidatesValues.hpp"
+#include "Traits.hpp"
+#include "BinnedData.hpp"
+
+//get_minimum_UB_offset_sign
+template<typename Offset, typename Sign> double get_minimum_UB_offset_sign(const BinnedDataCore& binned);
+
+//get_minimum_UB_degen
+double get_max_forbidden_border(const BinnedDataCore& binned, const Rcpp::NumericVector& beta);
+template<typename Degeneracy> double get_minimum_UB_degen(const BinnedDataCore& binned, const Rcpp::NumericVector& borders);
+
+//get_minimum_UB
+template<typename Offset, //whether offset should be held at zero (ZeroOffset) or estimated (EstimatedOffset)
+         typename Sign, //whether there is no constraint on the sign of the estimate (AnySign) or whether it must be positive (PositiveSign)
+         typename Degeneracy> //whether to forbid certain values (ForbidDegeneracy) in order to avoid degeneracies in the model, or not (AllowDegeneracy)
+double get_minimum_UB(const BinnedDataCore& binned) {
+    const CandidatesValues<Offset,Sign> cv(binned);
+    const Rcpp::NumericVector values = cv.get();
+    const double minUBo = get_minimum_UB_offset_sign<Offset,Sign>(binned);
+    const double minUBd = get_minimum_UB_degen<Degeneracy>(binned, values);
+    const double minUB = std::max(minUBd,minUBo);
+    return minUB;
+}
+
+#endif
+
