93d044247eb9f5fd7073a0d2e822719878ea7118
Yannick Spill <yannick.spill@crg.eu>
2017-10-07 09:43:02 +0200

    put FastData in separate source files

---
+++ csnorm/src/FastData.cpp
@@ -0,0 +1,83 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include "FastData.hpp"
+
+
+FastData::FastData(const DataFrame& obs, unsigned nbins) :
+name_(Rcpp::as<std::vector<unsigned> >(obs["name"])),
+bin1_(Rcpp::as<std::vector<unsigned> >(obs["bin1"])),
+bin2_(Rcpp::as<std::vector<unsigned> >(obs["bin2"])),
+observed_(Rcpp::as<std::vector<unsigned> >(obs["observed"])),
+nbins_(nbins), ncells_(nbins*(nbins+1)/2), ndatasets_(*std::max_element(name_.begin(), name_.end())), N_(ndatasets_*ncells_),
+log_biases_(std::vector<double>(nbins_,0)),
+log_decay_(std::vector<double>(nbins_,0)),
+log_signal_(std::vector<double>(N_,0)),
+phihat_(std::vector<double>(N_,0)),
+weights_(std::vector<double>(N_,0)),
+exposures_(std::vector<double>(ndatasets_,0)) {
+    if (name_.size() != N_) Rcpp::stop("Input size should be N(N+1)/2 dense matrix of observed counts for each dataset");
+    if (*std::min_element(name_.begin(), name_.end()) != 1) Rcpp::stop("Name column values should start at 1");
+    if (*std::min_element(bin1_.begin(), bin1_.end()) != 1 || *std::min_element(bin2_.begin(), bin2_.end()) != 1)
+      Rcpp::stop("bins should range from 1 to the number of bins");
+    if (*std::max_element(bin1_.begin(), bin1_.end()) != nbins_ || *std::max_element(bin2_.begin(), bin2_.end()) != nbins_)
+      Rcpp::stop("bins should range from 1 to the number of bins");
+}
+
+std::vector<double> FastData::get_log_expected() const {
+    std::vector<double> log_expected;
+    log_expected.reserve(get_N());
+    for (unsigned i=0; i<get_N(); ++i) {
+        unsigned bin1 = bin1_[i]-1; //offset by 1 for vector indexing
+        unsigned bin2 = bin2_[i]-1;
+        double bi = log_biases_[bin1];
+        double bj = log_biases_[bin2];
+        double decay = log_decay_[bin2-bin1];
+        double signal = log_signal_[i];
+        unsigned name = name_[i]-1;
+        double exposure = exposures_[name];
+        unsigned observed = observed_[i];
+        log_expected.push_back(bi + bj + decay + signal + exposure);
+    }
+    return log_expected;
+}
+ 
+Rcpp::DataFrame FastData::get_as_dataframe() const {
+    //bias, decay, signal with decay and exposures, and expected matrix (w/ offset)
+    std::vector<double> biasmat,decaymat,binless,expected;
+    biasmat.reserve(get_N());
+    decaymat.reserve(get_N());
+    binless.reserve(get_N());
+    for (unsigned i=0; i<get_N(); ++i) {
+        unsigned bin1 = bin1_[i]-1; //offset by 1 for vector indexing
+        unsigned bin2 = bin2_[i]-1;
+        double bi = log_biases_[bin1];
+        double bj = log_biases_[bin2];
+        biasmat.push_back(bi + bj);
+        double decay = log_decay_[bin2-bin1];
+        decaymat.push_back(decay);
+        double signal = log_signal_[i];
+        unsigned name = name_[i]-1;
+        double exposure = exposures_[name];
+        binless.push_back(decay + signal);
+        expected.push_back(std::exp(bi + bj + decay + signal + exposure));
+    }
+    return Rcpp::DataFrame::create(_["name"]=name_,
+                                   _["bin1"]=bin1_,
+                                   _["bin2"]=bin2_,
+                                   _["observed"]=observed_,
+                                   _["expected"]=expected,
+                                   _["biases"]=biasmat,
+                                   _["decay"]=decaymat,
+                                   _["signal"]=log_signal_,
+                                   _["binless"]=binless,
+                                   _["phihat"]=phihat_,
+                                   _["weights"]=weights_ );
+}
+
+
+
+
--- csnorm/src/fast_binless.hpp
+++ csnorm/src/fast_binless.hpp
@@ -4,52 +4,9 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 #include <vector>
+#include "FastData.hpp"
 #include "GFLLibrary.hpp"
 #include "FusedLassoGaussianEstimator.hpp"
-
-class FastData {
-public:
-    FastData(const DataFrame& obs, unsigned nbins);
-    
-    unsigned get_N() const { return N_; }
-    unsigned get_nbins() const { return nbins_; }
-    unsigned get_ncells() const { return ncells_; }
-    unsigned get_ndatasets() const { return ndatasets_; }
-    std::vector<unsigned> get_name() const { return name_; }
-    std::vector<unsigned> get_bin1() const { return bin1_; }
-    std::vector<unsigned> get_bin2() const { return bin2_; }
-    std::vector<unsigned> get_observed() const { return observed_; }
-     
-    std::vector<double> get_log_biases() const { return log_biases_; }
-    void set_log_biases(const std::vector<double> log_biases) { log_biases_ = log_biases; }
-     
-    std::vector<double> get_log_decay() const { return log_decay_; }
-    void set_log_decay(const std::vector<double> log_decay) { log_decay_ = log_decay; }
-
-    std::vector<double> get_log_signal() const { return log_signal_; }
-    void set_log_signal(const std::vector<double> log_signal) { log_signal_ = log_signal; }
-    
-    std::vector<double> get_signal_phihat() const { return phihat_; }
-    void set_signal_phihat(const std::vector<double> phihat) { phihat_ = phihat; }
-    
-    std::vector<double> get_signal_weights() const { return weights_; }
-    void set_signal_weights(const std::vector<double> weights) { weights_ = weights; }
-
-    std::vector<double> get_exposures() const { return exposures_; }
-    void set_exposures(const std::vector<double> exposures) { exposures_ = exposures; }
-    
-    std::vector<double> get_log_expected() const;
-    
-    Rcpp::DataFrame get_as_dataframe() const;
-
-private:
-    const std::vector<unsigned> name_,bin1_,bin2_,observed_; //N(N+1)/2
-    const unsigned nbins_, ncells_, ndatasets_, N_;
-    std::vector<double> log_biases_,log_decay_; //N
-    std::vector<double> log_signal_,phihat_,weights_; //N(N+1)/2
-    std::vector<double> exposures_; //ndatasets
-};
-
 
 struct ResidualsPair { std::vector<double> residuals,weights; };
 struct SignalTriplet { std::vector<double> phihat, weights, beta; };
