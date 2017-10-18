3fb5046088ecf9fc845e6de04005306846124c89
Yannick Spill <yannick.spill@crg.eu>
2017-10-18 11:48:34 +0200

    remove non-public source code

--- csnorm/R/zzz.R
+++ csnorm/R/zzz.R
@@ -1,5 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  modules <- paste0("stan_fit4", names(stanmodels), "_mod")
-  for (m in modules) loadModule(m, what = TRUE)
   loadModule("csnorm_cpp", what = TRUE)
 }
--- csnorm/src/csnorm_cpp.cpp
+++ csnorm/src/csnorm_cpp.cpp
@@ -1,17 +1,11 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-#include "perf_iteration_signal.hpp"
-#include "perf_iteration_diff.hpp"
 #include "fast_binless.hpp"
 
 RCPP_MODULE(csnorm_cpp) {
     using namespace Rcpp ;
 
-    function("wgfl_signal_BIC", &wgfl_signal_BIC, "documentation for wgfl_signal_BIC ");
-    
-    function("wgfl_diff_BIC", &wgfl_diff_BIC, "documentation for wgfl_diff_BIC ");
-    
     function("fast_binless", &fast_binless, "documentation for fast_binless ");
     function("fast_binless_eval_cv", &fast_binless_eval_cv, "documentation for fast_binless_eval_cv ");
     function("fast_binless_difference", &fast_binless_difference, "documentation for fast_binless_difference ");
