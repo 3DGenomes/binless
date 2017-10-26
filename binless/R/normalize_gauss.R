92670dc596784a4d36dc82ceea040a68e232148d
Yannick Spill <yannick.spill@crg.eu>
2017-10-26 12:29:59 +0200

    report all computed precisions

--- binless/R/normalize_gauss.R
+++ binless/R/normalize_gauss.R
@@ -748,8 +748,8 @@ has_converged = function(cs) {
   conv.phi = rel.precision("phi")
   #cat(" conv.log_iota ", conv.log_iota,
   #    " conv.log_rho ", conv.log_rho, " conv.log_decay ", conv.log_decay, " conv.phi ", conv.phi, "\n")
-  cat(" mean relative precision for this iteration: ",
-      mean(c(conv.log_iota,conv.log_rho,conv.log_decay,conv.phi), na.rm=T), "\n")
+  cat(" relative precision for this iteration: iota ", conv.log_iota,
+      " rho ", conv.log_rho, " decay ", conv.log_decay, " signal ", conv.phi, "\n")
   conv.param = all(c(conv.log_iota,conv.log_rho,
                      conv.log_decay,conv.phi)<cs@settings$tol, na.rm=T)
   return(conv.param)
