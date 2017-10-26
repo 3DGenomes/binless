6ce5ea2f2bce15a67fb7cc29e1e1132e58ae759b
Yannick Spill <yannick.spill@crg.eu>
2017-10-26 18:48:17 +0200

    Revert "remove degeneracy after the fused lasso"
    This reverts commit eb45d6fe74559490bdc4a2924c25dbdcb5d03502.

--- rao.R
+++ rao.R
@@ -29,7 +29,7 @@ a=examine_dataset("/scratch/rao/mapped/IMR90_MboI_in_situ/IMR90_MboI_HICall_Peak
 registerDoParallel(cores=10)
 chrs=c("chrX","chr1","chr1","chr7","chr3","chr4","chr21","chr21","chr5","chr12","chr21","chr22","chr1","chr3","chr3")
 names=c("Peak1","SELP","Talk","SEMA3C","FOXP1","PARM1","Comparison","ADAMTS1","ADAMTS2","TBX3","Fig1C","22qter","Tbx19","FOXP1ext","FOXP1big")
-sizes=c("450k","150k","2M","1M","1.3M","600k","1.7M","2.3M","450k","1.5M","1M","1.7M","700k","2.3M","5M")
+sizes=c("450k","150k","2M","1M","1.3M","600k","1.7M","2.3M","450k","1.5M","1M","1.7M","2.3M","5M")
 foreach (chr=chrs, name=names, size=sizes) %dopar% {
            cat(chr,name,size,"\n")
            csd=read_and_prepare(paste0("zcat /scratch/rao/mapped/GM12878_MboI_in_situ/GM12878_MboI_HICall_",name,".tsv.gz"),
