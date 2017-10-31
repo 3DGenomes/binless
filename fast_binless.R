67a0099960c540cbfaefef20cff89fcc41eb4ffe
Yannick Spill <yannick.spill@crg.eu>
2017-10-31 12:14:27 +0100

    fast_binless tuto update

--- fast_binless.R
+++ fast_binless.R
@@ -30,72 +30,56 @@ mat[unclass(bin1)==1&unclass(bin2)==max(unclass(bin2))]
 #The fast binless algorithm computes a binless normalization without estimating
 #the fusion penalty and the significance threshold.
 #We do a maximum of nouter steps (or less if the relative precision is lower than tol_val)
-#and hold the fusion penalty fixed at lam2. Play with lam2 to see its effect.
-nouter=20
+#and hold the fusion penalty fixed at lam2.
+#Play with lam2 to see its effect. This is the parameter that is optimized in the
+#full-blown binless, along with another threshold (lambda1, set to zero here),
+#which determines the significance of a given signal/difference contribution
+nouter=25
 lam2=5
 tol_val=2e-1
 bg_steps=5
+setwd("..")
+mat=fread("gzcat ../binless/example/rao_HiCall_FOXP1ext_2.3M_mat_5kb.dat.gz",stringsAsFactors = T)
 out=binless:::fast_binless(mat, mat[,nlevels(bin1)], lam2, nouter, tol_val, bg_steps)
 
 
-#Here follow pretty much all the plots you could think of (be sure to check out signal and binless)
+#Here follow the plots of the observed and fitted quantities (be sure to check out signal and binless plots)
 #all data
 a=as.data.table(out$mat)
-#observed matrix
-plot_binless_matrix(out$mat, upper="log(observed)", lower="log(observed)")
-ggplot(out$mat)+geom_raster(aes(bin1,bin2,fill=log(observed)))+geom_raster(aes(bin2,bin1,fill=log(observed)))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"), na.value = "white")
-#expected matrix
-ggplot(out$mat)+geom_raster(aes(bin1,bin2,fill=log(expected)))+geom_raster(aes(bin2,bin1,fill=log(expected)))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))
+#observed matrix (input data)
+plot_binless_matrix(a, upper="log(observed)", lower="log(observed)")
+#fitted background
+plot_binless_matrix(a, upper="log_background", lower="log_background")
 #fitted biases
 ggplot(data.table(bin=1:nlevels(mat[,bin1]),log_biases=out$log_biases))+geom_point(aes(bin,log_biases,colour="cpp"))#+
 #biases matrix
-ggplot(out$mat)+geom_raster(aes(bin1,bin2,fill=biases))+geom_raster(aes(bin2,bin1,fill=biases))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))
+plot_binless_matrix(a, upper="log_biases", lower="log_biases")
 #fitted decay
 ggplot(data.table(distance=1:nlevels(mat[,bin1]),log_decay=out$log_decay))+geom_point(aes(distance,log_decay,colour="cpp"))#+
 #decay matrix
-ggplot(out$mat)+geom_raster(aes(bin1,bin2,fill=decay))+geom_raster(aes(bin2,bin1,fill=decay))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))
-#signal matrix
-ggplot(out$mat)+geom_raster(aes(bin1,bin2,fill=signal))+geom_raster(aes(bin2,bin1,fill=signal))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))
-#signal and phihat
-ggplot(as.data.table(out$mat))+geom_raster(aes(bin1,bin2,fill=signal))+geom_raster(aes(bin2,bin1,fill=pmin(phihat,max(signal))))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))
-#signal and log(observed)-background
-ggplot(as.data.table(out$mat))+geom_raster(aes(bin1,bin2,fill=signal))+geom_raster(aes(bin2,bin1,fill=log(observed/expected)+signal))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"), na.value = "white")
-#weights
-ggplot(as.data.table(out$mat))+geom_raster(aes(bin1,bin2,fill=log(weights)))+geom_raster(aes(bin2,bin1,fill=log(weights)))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))
-#binless matrix
-ggplot(out$mat)+geom_raster(aes(bin1,bin2,fill=binless))+geom_raster(aes(bin2,bin1,fill=binless))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))
+plot_binless_matrix(a, upper="log_decay", lower="log_decay")
+#signal matrix ( = what is different from the background)
+plot_binless_matrix(a, upper="log_signal", lower="log_signal")
+#weights ( = 1/variance )
+plot_binless_matrix(a, upper="log(weights)", lower="log(weights)")
+#binless matrix ( = signal + decay)
+plot_binless_matrix(a, upper="log_binless", lower="log_binless")
 #binless and observed
-ggplot(out$mat)+geom_raster(aes(bin1,bin2,fill=binless))+geom_raster(aes(bin2,bin1,fill=log(observed)*max(binless)/log(max(observed))))+
-  facet_wrap(~name)+coord_fixed()+scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"), na.value = "white")
+plot_binless_matrix(a, upper="log_binless", lower="log(observed)")
 #ggsave(filename="example/rao_HiCall_FOXP1ext_2.3M_binless_signal.pdf", width=18,height=8)
 
 
+### Difference detection
+
 #now we compute differences between the two datasets
 ref=1
-lam2=10
+lam2=5
 tol_val=2e-1
 diff=as.data.table(binless:::fast_binless_difference(out, lam2, ref, tol_val))
 
-#some plots (check out delta)
-#differences: log(observed)
-ggplot(diff)+geom_raster(aes(bin1,bin2,fill=log(observed)))+
-  geom_raster(aes(bin2,bin1,fill=log(observed)))+facet_wrap(~name)+
-  scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"),na.value = "white")+coord_fixed()
-#differences: phi_ref vs phihat
-ggplot(diff)+geom_raster(aes(bin1,bin2,fill=pmin(phi_ref,5)))+
-  geom_raster(aes(bin2,bin1,fill=pmin(phihat,5)),data=out$mat)+facet_wrap(~name)+
-  scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))+coord_fixed()
-#differences: delta
-ggplot(diff[name!=ref])+geom_raster(aes(bin1,bin2,fill=delta))+
-  geom_raster(aes(bin2,bin1,fill=delta))+facet_wrap(~name)+
-  scale_fill_gradient2(low=muted("blue"),mid="white",high=muted("red"))+coord_fixed()
+#log(observed)
+plot_binless_matrix(diff, upper="log(observed)", lower="log(observed)")
+#log difference of all datasets wrt ref
+plot_binless_matrix(diff[name!=ref], upper="log_difference", lower="log_difference")
 #ggsave(filename="example/rao_HiCall_FOXP1ext_2.3M_binless_difference.pdf", width=10,height=8)
+
