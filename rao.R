dfc907d2daf56980200973f2c2e79fb5533de468
Yannick Spill <yannick.spill@crg.eu>
2017-11-02 08:54:40 +0100

    new rao images

--- rao.R
+++ rao.R
@@ -27,10 +27,10 @@ a=examine_dataset("/scratch/rao/mapped/IMR90_MboI_in_situ/IMR90_MboI_HICall_Peak
 #they contain the respective CSdata objects
 
 registerDoParallel(cores=10)
-chrs=c("chrX","chr1","chr1","chr7","chr3","chr4","chr21","chr21","chr5","chr12","chr21","chr22","chr1","chr3","chr3")
-names=c("Peak1","SELP","Talk","SEMA3C","FOXP1","PARM1","Comparison","ADAMTS1","ADAMTS2","TBX3","Fig1C","22qter","Tbx19","FOXP1ext","FOXP1big")
-sizes=c("450k","150k","2M","1M","1.3M","600k","1.7M","2.3M","450k","1.5M","1M","1.7M","2.3M","5M")
-foreach (chr=chrs, name=names, size=sizes) %dopar% {
+chrs=c("chrX",  "chr1", "chr1", "chr7",   "chr3",  "chr4",  "chr21",      "chr21",   "chr5",    "chr12", "chr21", "chr22",  "chr1",  "chr3",     "chr3")
+names=c("Peak1","SELP", "Talk", "SEMA3C", "FOXP1", "PARM1", "Comparison", "ADAMTS1", "ADAMTS2", "TBX3",  "Fig1C", "22qter", "Tbx19", "FOXP1ext", "FOXP1big")
+sizes=c("450k", "150k", "2M",   "1M",     "1.3M",  "600k",  "1.7M",       "2.3M",    "450k",    "1.5M",  "1M",    "1.7M",   "700k",  "2.3M",     "5M")
+foreach (chr=chrs, name=names, size=sizes, .errorhandling="remove") %dopar% {
   cat(chr,name,size,"\n")
   if (F) {
     csd=read_and_prepare(paste0("zcat /scratch/rao/mapped/GM12878_MboI_in_situ/GM12878_MboI_HICall_",name,".tsv.gz"),
@@ -54,15 +54,15 @@ foreach (chr=chrs, name=names, size=sizes) %dopar% {
                            dangling.R=c(3), maxlen=600, read.len=101, dmin=1000, save.data=T)
     }
   }
-  for (run in c("01-9","10-18")) {
-    csd=read_and_prepare(paste0("zcat /scratch/rao/mapped/GM12878_MboI_in_situ/GM12878_MboI_HIC0",run,"_",name,".tsv.gz"),
-                         paste0("data/rao_HiC0",run,"_GM12878_",name,"_",size), "GM", run,
+  for (run in c("odd","even")) {
+    csd=read_and_prepare(paste0("zcat /scratch/rao/mapped/GM12878_MboI_in_situ/GM12878_MboI_HIC",run,"_",name,".tsv.gz"),
+                         paste0("data/rao_HiC",run,"_GM12878_",name,"_",size), "GM", run,
                          enzyme="MboI", name=paste(name,"GM12878",run), circularize=-1, dangling.L=c(0),
                          dangling.R=c(3), maxlen=900, read.len=101, dmin=1000, save.data=T)
   }
-  for (run in c("50-53","54-56")) {
-    csd=read_and_prepare(paste0("zcat /scratch/rao/mapped/IMR90_MboI_in_situ/IMR90_MboI_HIC0",run,"_",name,".tsv.gz"),
-                         paste0("data/rao_HiC0",run,"_IMR90_",name,"_",size), "IMR90", run,
+  for (run in c("odd","even")) {
+    csd=read_and_prepare(paste0("zcat /scratch/rao/mapped/IMR90_MboI_in_situ/IMR90_MboI_HIC",run,"_",name,".tsv.gz"),
+                         paste0("data/rao_HiC",run,"_IMR90_",name,"_",size), "IMR90", run,
                          enzyme="MboI", name=paste(name,"IMR90",run), circularize=-1, dangling.L=c(0),
                          dangling.R=c(3), maxlen=600, read.len=101, dmin=1000, save.data=T)
   }
