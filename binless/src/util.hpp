2aead8cbdd54e25b57cfca74d3a6634108b443b0
Yannick Spill <yannick.spill@crg.eu>
2017-12-27 11:45:30 +0100

    data binning now drops empty bins by default

--- binless/src/util.hpp
+++ binless/src/util.hpp
@@ -19,16 +19,24 @@ Rcpp::NumericVector compute_phi_ref(const BinnedData<Difference>& binned,
 
 double get_maximum_admissible_weight(const Rcpp::NumericVector& w, double percentile);
 
-//create mapping for evenly sized bins with labels from 0 to N. Form is begin -> bin_no.
+struct BinDesign {
+  unsigned Nbins;
+  std::map<double,unsigned> map;
+  std::vector<double> begins, ends;
+};
+
+//create design for evenly sized bins with labels from 0 to N. Form is begin -> bin_no.
 //Contains final bounday (size is N+1).
-std::map<double,unsigned> make_even_bins(double lower, double upper, unsigned Nbins);
+BinDesign make_even_bins(double lower, double upper, unsigned Nbins);
 
 //bin data given a boundary map in the form [a,b) [b,c) ... [y,z] with labels from 0 to N-1
-Eigen::SparseMatrix<unsigned> bin_data(const Eigen::VectorXd& data, const std::map<double,unsigned>& begin2bin);
+//Return in Nbins x Ndata sparse matrix form.
+//by default, drops unused bins (rows with only zeroes)
+Eigen::SparseMatrix<double, Eigen::RowMajor> bin_data(const Eigen::VectorXd& data, const BinDesign& begin2bin, bool drop = true);
 
 //Bin data evenly across Nbins bins and return in Nbins x Ndata sparse matrix form.
-Eigen::SparseMatrix<unsigned> bin_data_evenly(const Eigen::VectorXd& data, unsigned Nbins);
-
+//by default, drops unused bins (rows with only zeroes)
+Eigen::SparseMatrix<double, Eigen::RowMajor> bin_data_evenly(const Eigen::VectorXd& data, unsigned Nbins, bool drop = true);
 
 
 #endif
