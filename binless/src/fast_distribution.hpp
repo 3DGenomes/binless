2b65439cd40d34fe0f08c6786311e33c3500474e
Yannick Spill <yannick.spill@crg.eu>
2018-03-12 13:42:28 +0100

    add Distribution and Sampler classes

---
+++ binless/src/fast_distribution.hpp
@@ -0,0 +1,53 @@
+#ifndef FAST_DISTRIBUTION_HPP
+#define FAST_DISTRIBUTION_HPP
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+namespace binless {
+namespace fast {
+
+//distribution families
+struct Distribution {};
+
+struct NormalDistribution : public Distribution {
+  double sigma;
+};
+
+struct PoissonDistribution : public Distribution {};
+
+struct NegativeBinomialDistribution : public Distribution {
+  double alpha; // dispersion parameter (variance is mu + mu^2/alpha)
+};
+
+
+//methods to sample remaining parameters of distributions
+// any Sampler specialization must have three calls
+// - a constructor that stores a reference to the distribution
+// - one init call to set the parameter to a sensible value
+// - one sample call, to update their value
+template<typename Dist> class Sampler {};
+
+template<> class Sampler<PoissonDistribution> {
+public:
+  Sampler(PoissonDistribution& nb) : nb_(nb) {}
+  void init() {}
+  void sample() {}
+private:
+  PoissonDistribution& nb_;
+};
+
+template<> class Sampler<NegativeBinomialDistribution> {
+public:
+  Sampler(NegativeBinomialDistribution& nb) : nb_(nb) {}
+  void init() { nb_.alpha = 1; }
+  void sample() {}
+private:
+  NegativeBinomialDistribution& nb_;
+};
+
+}
+}
+
+#endif
+
