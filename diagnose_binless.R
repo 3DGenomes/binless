f8bfd2dd633ce171b1c4ab182848b4370621556e
Yannick <yannick.spill@crg.eu>
2018-04-23 17:49:42 +0200

    diagnostics

--- diagnose_binless.R
+++ diagnose_binless.R
@@ -43,34 +43,29 @@ ggplot(signals[name==unique(name)[1],.(step,value=beta-shift(beta)),by=c("name",
 ggplot(signals[bin1==min(bin1)])+geom_line(aes(bin2,phi,group=name))+geom_point(aes(bin2,phihat),alpha=0.1)+facet_grid(step~name)+ylim(-2.5,10)
 
 #biases vs step
-biases.ori=merge(rbindlist(lapply(cs.ori@diagnostics$params[leg=="bias",log_iota],function(x){cs@biases[,.(name,pos,log_iota=x)]}),use=T,id="step"),
-             rbindlist(lapply(cs.ori@diagnostics$params[leg=="bias",log_rho],function(x){cs@biases[,.(name,pos,log_rho=x)]}),use=T,id="step"))[name==name[1]]
-biases.new=rbindlist(cs.new@diagnostics$params[leg=="bias",biases],use=T,id="step")[group==group[1]&cat%in%c("contact L","contact R")]
-biases.new=merge(biases.new[cat=="contact L",.(step,pos,log_iota=eta)],biases.new[cat=="contact R",.(step,group,pos,log_rho=eta)],by=c("step","pos"))
-biases=melt(rbindlist(list(new=biases.new[,.(step,pos,log_iota,log_rho)],ori=biases.ori[,.(step,pos,log_iota,log_rho)]),use=T,idcol="origin"),id=c("origin","step","pos"))
-ggplot(biases)+geom_line(aes(pos,value,colour=origin))+facet_grid(step~variable)
+biases=rbindlist(cs.new@diagnostics$params[leg=="bias",biases],use=T,id="step")[cat%in%c("contact L","contact R")]
+biases=rbind(biases[cat=="contact L",.(step,bias="log_iota",group,pos,eta,etahat,std,nobs)],
+             biases[cat=="contact R",.(step,bias="log_rho",group,pos,eta,etahat,std,nobs)])
+ggplot(biases)+geom_line(aes(pos,eta))+geom_point(aes(pos,etahat),alpha=0.1)+
+  facet_grid(step~group+bias)
 
 #biases differential
-ggplot(biases[,.(step,value=value-shift(value)),by=c("name","pos","variable")])+geom_line(aes(pos,value,colour=variable))+facet_grid(step~variable)
+ggplot(biases[,.(step,eta=eta-shift(eta)),by=c("group","pos","bias")])+geom_line(aes(pos,eta,colour=bias))+facet_grid(step~group+bias)
 
 #decay vs step (x log scale)
-decays.ori=rbindlist(cs.ori@diagnostics$params[leg=="decay",decay],idcol = "step", use=T)
-decays.ori[,group:=factor(unclass(name))]
-decays.ori[,name:=NULL]
-decays.new=rbindlist(cs.new@diagnostics$params[leg=="decay",decay],idcol = "step", use=T)
-decays.new[,group:=factor(group)]
-decays=rbindlist(list(ori=decays.ori,new=decays.new),use=T,id="origin")
-ggplot(decays[,.(origin,distance,log_decay,std,kappahat),by=c("group","step")])+
-  geom_line(aes(distance,log_decay,group=origin,colour=origin))+geom_point(aes(distance,kappahat,colour=origin),alpha=0.1)+
-  geom_errorbar(aes(distance,ymin=kappahat-std,ymax=kappahat+std,colour=origin),alpha=0.1)+facet_grid(group~step)+scale_x_log10()
+decays=rbindlist(cs.new@diagnostics$params[leg=="decay",decay],idcol = "step", use=T)
+decays[,group:=factor(group)]
+ggplot(decays)+
+  geom_line(aes(distance,log_decay,group=group))+geom_point(aes(distance,kappahat,colour=group),alpha=0.1)+
+  geom_errorbar(aes(distance,ymin=kappahat-std,ymax=kappahat+std,colour=group),alpha=0.1)+facet_grid(group~step)+scale_x_log10()
 
 #decay vs step (x linear scale)
-ggplot(decays[,.(distance,log_decay,std,kappahat),by=c("group","step")])+
+ggplot(decays)+
   geom_line(aes(distance,log_decay,group=group))+geom_point(aes(distance,kappahat,colour=group),alpha=0.1)+
-  geom_errorbar(aes(distance,ymin=kappahat-std,ymax=kappahat+std,colour=group),alpha=0.1)+facet_wrap(~step)
+  geom_errorbar(aes(distance,ymin=kappahat-std,ymax=kappahat+std,colour=group),alpha=0.1)+facet_grid(group~step)
 
 #fit last decay to polymer model
-decay=cs@par$decay[name==name[1],.(ldist=log(distance),ldec=log_decay,distance,decay=exp(log_decay))]
+decay=cs@par$decay[group==group[1],.(ldist=log(distance),ldec=log_decay,distance,decay=exp(log_decay))]
 fit=lm(ldec~ldist,data=decay[distance>1e4&distance<5e5])
 summary(fit)
 decay[,model:=exp(fit$coefficients[["(Intercept)"]]+fit$coefficients[["ldist"]]*ldist)]
