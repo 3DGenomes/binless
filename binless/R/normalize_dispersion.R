0e80a676add7a571900f841a25626cdcdcc6eb3f
Yannick Spill <yannick.spill@crg.eu>
2018-05-01 20:58:59 +0200

    set minimum dispersion at 1e-2

--- binless/R/normalize_dispersion.R
+++ binless/R/normalize_dispersion.R
@@ -4,13 +4,14 @@ NULL
 #' Single-cpu simplified fitting for exposures and dispersion
 #' @keywords internal
 #' 
-gauss_dispersion = function(cs, cts.common, verbose=T) {
+gauss_dispersion = function(cs, cts.common, verbose=T, alpha.min=0.01) {
   if (verbose==T) cat(" Dispersion\n")
   #predict all means and put into table
   bts = binless:::gauss_common_muhat_mean_biases(cs)[,.(cat,count,mu,nobs)]
   cts = cts.common[,.(cat,count,mu,nobs=nobs/2)]
   data = rbind(bts,cts)
   alpha = MASS::theta.ml(data[,count], data[,mu], data[,sum(nobs)], data[,nobs], limit=10, eps=cs@par$tol_disp)
+  alpha = max(alpha,alpha.min)
   #restrict tolerance if needed
   precision = max(abs(alpha - cs@par$alpha))
   cs@par$tol_disp = min(cs@par$tol_disp, max(cs@settings$tol, precision/10))
