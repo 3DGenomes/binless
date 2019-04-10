f7f4385add8f7acfed5cae11b2cc807c2d619c63
Yannick Spill <yannick.spill@crg.eu>
2019-04-10 15:40:19 +0200

    update comments

--- chromosome_binless.R
+++ chromosome_binless.R
@@ -2,6 +2,11 @@
 
 library(binless)
 
+### Script to normalize a whole chromosome using a combination of optimized and fast binless
+### It is highly recommended you first go through preprocessing.R, optimized_binless.R and fast_binless.R
+
+
+
 args=commandArgs(trailingOnly=TRUE)
 if (length(args)<= 1 || length(args) > 3) stop(paste0("\nargs: resolution sample1 [sample2]\n"))
 resolution=as.integer(args[1])
@@ -13,17 +18,18 @@ if (length(args)==3) {
 }
 
 
-
-### fixed parameters
-
 ncores = 8
 far.cutoff=10e6 #distance at which we should stop computing signal 
                 #(used for final lambda1 estimation, to lower memory footprint,
                 #and to write matrices)
 out_prefix="chromosome_binless"
-#zoom_region="full"
-zoom_region=c(20000000,30000000)
+zoom_region="full"
+#zoom_region=c(20000000,30000000)
 
 chromosome_binless(sample, sample2=sample2, zoom_region=zoom_region, ncores=ncores, base.res=resolution,
-                   far.cutoff=far.cutoff, out_prefix="chromosome_binless")
+                   far.cutoff=far.cutoff, out_prefix=out_prefix)
+
+### the matrices produced can be loaded and plotted with the following commands
+#mat = read_chromosome_binless(sample, sample2=sample2, zoom_region=zoom_region, out_prefix=out_prefix)
+#plot_binless_matrix(mat)
 
